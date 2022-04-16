e9851a8d66945b813ce35dfe177f6989eb09e5e7
Naman Sharma <82465837+nxmxn-shxrmx@users.noreply.github.com>
2022-04-16 17:18:33 +0530

    Time: 47 ms (65.83%), Space: 33.3 MB (99.74%) - LeetHub

--- 538-convert-bst-to-greater-tree/538-convert-bst-to-greater-tree.cpp
+++ 538-convert-bst-to-greater-tree/538-convert-bst-to-greater-tree.cpp
@@ -11,32 +11,17 @@
  */
 class Solution {
 public:
-    int sum(TreeNode *root,int x)
-    {
-        if(root==NULL)
-            return 0;
-        if(root->val>x)
-            return sum(root->right,x)+root->val+sum(root->left,x);
-        
-        return sum(root->right,x);
-    }
-    TreeNode* sub(TreeNode* root,TreeNode* r)
-    {
-        if(r==NULL)
-            return NULL;
-        int x = r->val;
-        x+=sum(root,x);
-        TreeNode* u = new TreeNode(x);
-        u->left =  sub(root,r->left);
-        u->right = sub(root,r->right);
-        
-        return u;
-    }
+     int x = 0;
+
     TreeNode* convertBST(TreeNode* root) {
-        if(root==NULL)
-            return NULL;
+        if(root!=NULL)
+        {
         
-        return sub(root,root);
-        
+        convertBST(root->right);
+        x+=root->val;
+        root->val = x;
+        convertBST(root->left);
+        }
+        return root;
     }
 };
