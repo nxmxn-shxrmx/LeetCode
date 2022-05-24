53fb1a765e540336279eabeb6e4de5aa3f58467a
Naman Sharma <82465837+nxmxn-shxrmx@users.noreply.github.com>
2022-05-24 18:59:52 +0530

    Added solution - LeetHub

---
+++ Image Multiplication - GFG/image-multiplication.cpp
@@ -0,0 +1,129 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+/* A binary tree node has data, pointer to left child
+   and a pointer to right child */
+struct Node {
+    int data;
+    struct Node* left;
+    struct Node* right;
+};
+Node* newNode(int val) {
+    Node* temp = new Node;
+    temp->data = val;
+    temp->left = NULL;
+    temp->right = NULL;
+    return temp;
+}
+Node* buildTree(string str) {
+    // Corner Case
+    if (str.length() == 0 || str[0] == 'N') return NULL;
+
+    // Creating vector of strings from input
+    // string after spliting by space
+    vector<string> ip;
+
+    istringstream iss(str);
+    for (string str; iss >> str;) ip.push_back(str);
+
+    // Create the root of the tree
+    Node* root = newNode(stoi(ip[0]));
+
+    // Push the root to the queue
+    queue<Node*> queue;
+    queue.push(root);
+
+    // Starting from the second element
+    int i = 1;
+    while (!queue.empty() && i < ip.size()) {
+
+        // Get and remove the front of the queue
+        Node* currNode = queue.front();
+        queue.pop();
+
+        // Get the current node's value from the string
+        string currVal = ip[i];
+
+        // If the left child is not null
+        if (currVal != "N") {
+
+            // Create the left child for the current node
+            currNode->left = newNode(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->left);
+        }
+
+        // For the right child
+        i++;
+        if (i >= ip.size()) break;
+        currVal = ip[i];
+
+        // If the right child is not null
+        if (currVal != "N") {
+
+            // Create the right child for the current node
+            currNode->right = newNode(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+
+    return root;
+}
+
+ // } Driver Code Ends
+/* Tree node structure  used in the program
+struct Node
+{
+    int data;
+    struct Node* left;
+    struct Node* right;
+
+    Node(int x){
+        data = x;
+        left = right = NULL;
+    }
+}; */
+class Solution
+{
+    public:
+    long long k = 0;
+    long long mod = 1e9 +7;
+    void in(Node* r1,Node* r2)
+    {
+        if(r1==NULL || r2==NULL)
+        return;
+        
+        k = k%mod +( (r1->data)%mod *  (r2->data)%mod ) %mod; 
+        k%=mod;
+        in(r1->left,r2->right);
+        in(r1->right,r2->left);
+    }
+    long long imgMultiply(Node *root)
+    {
+        k+=(root->data*root->data);
+        in(root->left,root->right);
+        return k;
+        // code here
+    }
+};
+
+// { Driver Code Starts.
+int main() {
+    int t;
+    scanf("%d\n", &t);
+    while (t--) {
+        string s;
+        getline(cin, s);
+        Node* root = buildTree(s);
+        Solution obj;
+        long long answer=obj.imgMultiply(root);
+        cout<<answer<<endl;
+    }
+    return 0;
+}
+  // } Driver Code Ends
