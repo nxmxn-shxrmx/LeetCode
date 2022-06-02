6681b47af35b160bf08841f35fa14a097c5825e1
Naman Sharma <82465837+nxmxn-shxrmx@users.noreply.github.com>
2022-06-02 21:55:39 +0530

    Added solution - LeetHub

---
+++ Construct Tree from Preorder Traversal - GFG/construct-tree-from-preorder-traversal.cpp
@@ -0,0 +1,81 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+#include <stdio.h>
+#include <stdlib.h>
+using namespace std;
+
+struct Node
+{
+	int data;
+	struct Node *left;
+	struct Node *right;
+	
+	Node(int x){
+	    data = x;
+	    left = NULL;
+	    right = NULL;
+	}
+};
+
+void printInorder (struct Node* node)
+{
+	if (node == NULL)return;
+	printInorder(node->left);
+	printf("%d ", node->data);
+	printInorder (node->right);
+}
+
+Node* constructTree(int n, int pre[], char preLN[]);
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        Node* root = NULL;
+        int n;
+        cin>>n;
+        int pre[n];
+        char preLN[n];
+        for(int i=0; i<n; i++)cin>>pre[i];
+        for(int i=0; i<n; i++)cin>>preLN[i];
+        root = constructTree (n, pre, preLN);
+        printInorder(root);
+        cout<<endl;
+    }
+	return 0;
+}
+// } Driver Code Ends
+
+
+/*Structure of the Node of the binary tree is as
+struct Node
+{
+	int data;
+	struct Node *left;
+	struct Node *right;
+};
+*/
+// function should return the root of the new binary tree formed
+ int i=0;
+Node* hel(int n,int pre[],char ln[])
+{
+     if(i>=n)
+    return NULL;
+    
+    Node* r = new Node(pre[i]);
+    ++i;
+    if(ln[i-1]=='L')
+    return r;
+
+    r->left = hel(n,pre,ln);
+    r->right= hel(n,pre,ln);
+    return r;
+}
+struct Node *constructTree(int n, int pre[], char preLN[])
+{
+    i =0;
+   return hel(n,pre,preLN);
+    
+}
