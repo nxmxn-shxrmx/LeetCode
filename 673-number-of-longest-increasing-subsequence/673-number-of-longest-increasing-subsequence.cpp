dc9eb6bb53a158504e0e6c47801e199053c346d2
Naman Sharma <82465837+nxmxn-shxrmx@users.noreply.github.com>
2022-06-07 00:19:07 +0530

    Time: 240 ms (30.98%), Space: 13.2 MB (85.26%) - LeetHub

--- 673-number-of-longest-increasing-subsequence/673-number-of-longest-increasing-subsequence.cpp
+++ 673-number-of-longest-increasing-subsequence/673-number-of-longest-increasing-subsequence.cpp
@@ -1,21 +1,36 @@
 class Solution {
 public:
      int findNumberOfLIS(vector<int>& nums) {
-        int n = nums.size(), res = 0, max_len = 0;
-        vector<pair<int,int>> dp(n,{1,1});            //dp[i]: {length, number of LIS which ends with nums[i]}
-        for(int i = 0; i<n; i++){
-            for(int j = 0; j <i ; j++){
-                if(nums[i] > nums[j]){
-                    if(dp[i].first == dp[j].first + 1)dp[i].second += dp[j].second;
-                    if(dp[i].first < dp[j].first + 1)dp[i] = {dp[j].first + 1, dp[j].second};
-                }
-            }
-            if(max_len == dp[i].first)res += dp[i].second;
-            if(max_len < dp[i].first){
-                max_len = dp[i].first;
-                res = dp[i].second;
-            }
-        }
-        return res;
+      vector<int>dp(nums.size());
+         vector<int>l(nums.size());
+         int c= 0;
+         for(int i = 0;i<nums.size();++i)
+         {
+             dp[i] =1;
+             l[i]=1;
+             for(int j = 0;j<i;++j)
+             {
+                 if(nums[j]<nums[i])
+                 {
+                     if(dp[j]+1>dp[i])
+                     {
+                         dp[i] = dp[j]+1;
+                         l[i]=l[j];
+                     }
+                     else if(dp[j]+1==dp[i])
+                         l[i]+=l[j];
+                 }
+             }
+             c = max(c,dp[i]);
+         }
+         
+         long long ans =0;
+         for(int i = 0;i<dp.size();++i)
+         {
+             if(dp[i]==c)
+                 ans+=l[i];
+         }
+         return ans;
+         
     }
 };
