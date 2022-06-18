af904f9fd9ad601c4eac13bfedbca4f8b143c1f6
Naman Sharma <82465837+nxmxn-shxrmx@users.noreply.github.com>
2022-06-19 00:36:29 +0530

    Time: 199 ms (41.01%), Space: 29.8 MB (80.05%) - LeetHub

--- 1926-nearest-exit-from-entrance-in-maze/1926-nearest-exit-from-entrance-in-maze.cpp
+++ 1926-nearest-exit-from-entrance-in-maze/1926-nearest-exit-from-entrance-in-maze.cpp
@@ -2,42 +2,35 @@ class Solution {
 public:
     int nearestExit(vector<vector<char>>& g, vector<int>& e) {
        
-        queue<pair<pair<int,int>,int>>q;
-        q.push({{e[0],e[1]},0});
+        queue<pair<int,int>>q;
+        q.push({e[0],e[1]});
         int st =1;
-        // for(auto c:g)
-        // {
-        //     for(auto x:c)
-        //         cout<<x<<" ";
-        //     cout<<"\n";
-        // }
         g[e[0]][e[1]]='+';
         int d[4][2] = {{0,1},{-1,0},{1,0},{0,-1}};
-        int cz = INT_MAX;
         
         while(!q.empty())
         {
-        
-            int i = q.front().first.first;
-            int j = q.front().first.second;
-            int k = q.front().second;
-            q.pop();
-            for(auto c:d)
+            int s = q.size();
+            while(s--)
             {
-                int ni =i+c[0];
-                int nj = j+c[1];
-                if(ni<0 || nj<0 || ni>=g.size() ||nj>=g[0].size() || g[ni][nj]=='+')
-                        continue;
-                if(ni==g.size()-1 || nj==g[0].size()-1 || nj==0 || ni==0)
-                {
+                int i = q.front().first;
+                int j = q.front().second;
 
-                    if(k+1<cz)
-                        cz  =k+1;
-                }
+                q.pop();
+                for(auto c:d)
+                {
+                    int ni =i+c[0];
+                    int nj = j+c[1];
+                    if(ni<0 || nj<0 || ni>=g.size() ||nj>=g[0].size() || g[ni][nj]=='+')
+                        continue;
+                    if(ni==g.size()-1 || nj==g[0].size()-1 || nj==0 || ni==0)
+                        return st;
                     g[ni][nj]='+';
-                    q.push({{ni,nj},k+1});
+                    q.push({ni,nj});
                 }
+            }
+            st++;
         }
-        return cz==INT_MAX?-1:cz;
+        return -1;
     }
 };
