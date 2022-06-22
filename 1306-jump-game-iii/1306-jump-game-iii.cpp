8b8c3cd82668032d68dc492ed34158e0cb2cf7b9
Naman Sharma <82465837+nxmxn-shxrmx@users.noreply.github.com>
2022-06-22 20:28:39 +0530

    Time: 56 ms (73.89%), Space: 35.5 MB (34.72%) - LeetHub

--- 1306-jump-game-iii/1306-jump-game-iii.cpp
+++ 1306-jump-game-iii/1306-jump-game-iii.cpp
@@ -3,18 +3,11 @@ public:
   
     bool canReach(vector<int>& arr, int s) {
         
-        if(s>=arr.size() || s<0)
+        if(s>=arr.size() || s<0 || arr[s]<0)
             return false;
         
-        if(arr[s]==0)
-            return true;
-        
-        if(arr[s]==-1)
-            return false;
-        
-        int h = arr[s];
-        arr[s] = -1;
-        return canReach(arr,s+h)||canReach(arr,s-h);
+        arr[s] = -arr[s];
+        return arr[s]==0||canReach(arr,s-arr[s])||canReach(arr,s+arr[s]);
         
     }
 };
