72e70e9ae4d315809a0f2ec875750b66b916c490
Naman Sharma <82465837+nxmxn-shxrmx@users.noreply.github.com>
2022-06-27 22:26:39 +0530

    Time: 111 ms (5.18%), Space: 93.3 MB (7.92%) - LeetHub

--- 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.cpp
@@ -1,29 +1,14 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& n, vector<int>& m) {
-        
-        vector<int>v;
-        
-        int i = 0;
-        int j = 0;
-        
-        while(i<n.size() && j<m.size())
-        {
-            if(n[i]>m[j])
-                v.push_back(m[j++]);
-            else
-                v.push_back(n[i++]);
-        }
-        while(i<n.size())
-            v.push_back(n[i++]);
-        while(j<m.size())
-            v.push_back(m[j++]);
-        
+        multiset<int>s(n.begin(),n.end());
+        s.insert(m.begin(),m.end());
+        vector<int>v(s.begin(),s.end());
+    
         if(v.size()%2)
-            return v[(v.size())/2];
+            return v[v.size()/2];
         
-        double h = (v[v.size()/2]+v[(v.size())/2 - 1]);
-//        cout<<v[v.size()/2]<<" "<<v[v.size()/2 -1]<<"\n";
+        double h = v[v.size()/2]+v[v.size()/2-1];
         return h/2.000;
     }
 };
