d46d5aca2a535e178ec8f4bc05ec7bac781bd2de
Naman Sharma <82465837+nxmxn-shxrmx@users.noreply.github.com>
2022-06-29 00:56:25 +0530

    Time: 1108 ms (5.05%), Space: 24.6 MB (5.01%) - LeetHub

--- 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.cpp
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.cpp
@@ -2,40 +2,29 @@ class Solution {
 public:
     int minDeletions(string s) {
         
-        map<int,set<char>>m;
+        map<int,int>m;
         map<char,int>u;
         for(auto c:s)
         {
             if(u[c]!=0)
             {
-                m[u[c]].erase(c);
+                m[u[c]]--;
                 u[c]++;
-                m[u[c]].insert(c);
+                m[u[c]]++;
                 
             }
             else 
-                u[c]++,m[u[c]].insert(c);
+                u[c]++,m[u[c]]++;
         }
         int c=0;
-        // for(auto c:m)
-        // {
-        //     cout<<c.first<<"->";
-        //     for(auto x:c.second)
-        //         cout<<x<<" ";
-        //     cout<<"\n";
-        // }
-        for(auto it =m.rbegin();it!=m.rend();++it)
+        for(auto it=m.rbegin();it!=m.rend();++it)
         {
-            while(it->second.size()>1)
-            {
-                auto g = it->second.begin();
-                if(it->first-1!=0)
-                m[it->first-1].insert(*g);
-                it->second.erase(*g);
-                c++;
-            }
-            
+            if(m[it->first]>1)
+                c+=(it->second-1);
+            if(it->first-1!=0 && m[it->first]>1)
+            m[it->first-1]+=m[it->first]-1;
         }
+    
         return c;
         
     }
