c2515f744b39661b45b42823b6b291b36ab5c11e
Naman Sharma <82465837+nxmxn-shxrmx@users.noreply.github.com>
2022-08-17 09:55:19 +0530

    Time: 378 ms (58.85%), Space: 68 MB (33.43%) - LeetHub

--- 815-bus-routes/815-bus-routes.cpp
+++ 815-bus-routes/815-bus-routes.cpp
@@ -1,40 +1,44 @@
 class Solution {
 public:
-    unordered_set<int>p;
-    unordered_map<int,unordered_set<int>>m;
-
-    int numBusesToDestination(vector<vector<int>>& g, int s, int t) {
-        
-        if(s==t)
-            return 0;
-        for(int i = 0;i<g.size();++i)
-        {
-            for(auto c:g[i])
-                m[c].insert(i);
-        }
     
-        queue<pair<int,int>>q;
-        q.push({s,0});
-        p.insert(s);
+    unordered_map<int,set<int>>m;
+    unordered_map<int,bool>v;
+    int numBusesToDestination(vector<vector<int>>& r, int s, int t) {
+        
+        for(int i = 0;i<r.size();++i)
+        {
+            for(auto c:r[i])
+            m[c].insert(i);
+        }
+        
+        
+        queue<int>q;
+        q.push(s);
+        int sq = 1;
         while(!q.empty())
         {
-            auto it = q.front();
-            if(it.first==t)
-                return it.second;
-            q.pop();
-            for(auto c:m[it.first])
+            int sz= q.size();
+            while(sz--)
             {
-                for(auto x:g[c])
+                int p = q.front();
+                if(p==t)
+                    return sq-1;
+                q.pop();
+                for(auto c:m[p])
                 {
-                    if(p.find(x)==p.end())
+                    if(!v[c])
                     {
-                        q.push({x,it.second+1});
-                        p.insert(x);
+                        v[c]=true;
+                        for(auto u:r[c])
+                        {
+                            q.push(u);
+                        }
                     }
                 }
-                g[c].clear();
             }
+             sq++;  
         }
-        return -1;
+            
+            return -1;
     }
 };
