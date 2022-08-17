2b468fbe18bd161d6b4ed9836546863379c1193b
Naman Sharma <82465837+nxmxn-shxrmx@users.noreply.github.com>
2022-08-17 10:35:04 +0530

    Time: 0 ms (100.00%), Space: 7.8 MB (48.72%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -1,38 +1,16 @@
 class Solution {
 public:
-    // vector<int>m;
-    // int sub(vector<int>&n,int i)
-    // {
-    //     if(i>=n.size())
-    //         return 0;
-    //     if(m[i]>=0)
-    //         return m[i];
-    //     int r =  max(sub(n,i+2)+n[i],sub(n,i+1));
-    //     m[i]= r;
-    //     return r;
-    // }
-    int rob(vector<int>& n) {
-//         if(n.size()==1)
-//             return n[0];
-        
-//         m = vector<int>(n.size()+1,-1);
-//         int u =sub(n,0);
-//         for(auto c:m)
-//             cout<<c<<" ";
-//         cout<<"\n";
-//         return u;
-        int a = 0;
-        int b = 0;
-        
-        for(int i = 0;i<n.size();++i)
+    int rob(vector<int>& nums) {
+        long long a=0,b=0;
+        for(int i = 0;i<nums.size();++i)
         {
-            if(i%2==0)
+            if(i&1)
             {
-                a = max(a+n[i],b);
+                a = max(nums[i]+a,b);
             }
             else
             {
-                b = max(b+n[i],a);
+                b =max(nums[i]+b,a);
             }
         }
         return max(a,b);
