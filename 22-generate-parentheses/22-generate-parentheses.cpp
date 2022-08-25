063d0530766661a45ac074588afc7304ded7329f
Naman Sharma <82465837+nxmxn-shxrmx@users.noreply.github.com>
2022-08-25 13:36:33 +0530

    Time: 18 ms (11.59%), Space: 18.9 MB (8.49%) - LeetHub

--- 22-generate-parentheses/22-generate-parentheses.cpp
+++ 22-generate-parentheses/22-generate-parentheses.cpp
@@ -1,14 +1,18 @@
 class Solution {
 public:
     vector<string>v;
-
+    map<string,bool>m;
     void reus(int i,int l,int r,string s,int n)
     {
         if(i==2*n)
         {
+            m[s]=1;
             v.push_back(s);
             return;
         }
+        if(m[s])
+            return;
+        m[s]=1;
         if(l<n)
             reus(i+1,l+1,r,s+"(",n);
         if(r<l && r<n)
