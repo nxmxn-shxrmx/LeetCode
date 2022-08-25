ccca44f1d0a26a7599a647f55e194cabd9fe36e8
Naman Sharma <82465837+nxmxn-shxrmx@users.noreply.github.com>
2022-08-25 13:31:12 +0530

    Time: 12 ms (24.27%), Space: 14 MB (29.63%) - LeetHub

---
+++ 22-generate-parentheses/22-generate-parentheses.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<string>v;
+    void reus(int i,int l,int r,string s,int n)
+    {
+        if(i==2*n)
+        {
+            v.push_back(s);
+            return;
+        }
+        if(l<n)
+            reus(i+1,l+1,r,s+"(",n);
+        if(r<l && r<n)
+            reus(i+1,l,r+1,s+")",n);
+
+    }
+    vector<string> generateParenthesis(int n) {
+        reus(0,0,0,"",n);
+        return v;
+    }
+};
