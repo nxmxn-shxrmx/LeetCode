213c262f02331854710673f334fe42b91f1b21f0
Naman Sharma <82465837+nxmxn-shxrmx@users.noreply.github.com>
2022-08-30 22:15:30 +0530

    Time: 10 ms (5.42%), Space: 7.2 MB (33.50%) - LeetHub

--- 48-rotate-image/48-rotate-image.cpp
+++ 48-rotate-image/48-rotate-image.cpp
@@ -2,29 +2,49 @@ class Solution {
 public:
     void rotate(vector<vector<int>>& m) {
         
-        int i =0;
-        while(i<m.size())
+//         int i =0;
+//         while(i<m.size())
+//         {
+//             int l =i;
+//             while(l<m.size())
+//             {
+//                 swap(m[l][i],m[i][l]);
+//                 ++l;
+//             }
+            
+//             ++i;
+//         }
+//         i = 0;
+//         int j = m.size()-1;
+//         while(i<j)
+//         {
+//             for(int k=0;k<m.size();++k)
+//             {
+//                 swap(m[k][i],m[k][j]);
+//             }
+//             ++i;
+//             --j;
+//         }
+        int n = m.size();
+        
+        
+        for(int i = 0;i<(n+1)/2;++i)
         {
-            int l =i;
-            while(l<m.size())
+            for(int j=0;j<n/2;++j)
             {
-                swap(m[l][i],m[i][l]);
-                ++l;
+                int t =  m[ n - 1 - j ][ i ];
+                
+                 m[ n - 1 - j ][ i ] = m[ n - 1 - i][n - 1 -j];
+                
+                m[n - 1 - i][ n - 1 - j]=m[ j ][ n - 1 - i];
+                
+                m[ j ][ n - 1 - i]=m[i][j];
+                
+                m[i][j]=t;
+                        
             }
             
-            ++i;
         }
-        i = 0;
-        int j = m.size()-1;
-        while(i<j)
-        {
-            for(int k=0;k<m.size();++k)
-            {
-                swap(m[k][i],m[k][j]);
-            }
-            ++i;
-            --j;
-        }
-        
+    
     }
 };
