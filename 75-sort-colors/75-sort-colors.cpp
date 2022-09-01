951bc0e135e652817d89297f8b3af50e66cfa8fa
Naman Sharma <82465837+nxmxn-shxrmx@users.noreply.github.com>
2022-09-02 04:15:10 +0530

    Time: 8 ms (11.81%), Space: 8.2 MB (70.44%) - LeetHub

--- 75-sort-colors/75-sort-colors.cpp
+++ 75-sort-colors/75-sort-colors.cpp
@@ -1,54 +1,14 @@
 class Solution {
 public:
-    void mer(vector<int>&v,int l,int m,int r)
-    {
-        int i = l;
-        int j = m+1;
-        vector<int>me;
-        while(i<=m && j<=r)
-        {
-            if(v[i]>=v[j])
-            {
-                me.push_back(v[j]);
-                ++j;
-            }
-            
-            else
-            {
-                me.push_back(v[i]);
-                ++i;
-            }
-                
-        }
-        while(i<=m)
-        {
-            me.push_back(v[i]);
-            ++i;
-        }
-         while(j<=r)
-        {
-            me.push_back(v[j]);
-            ++j;
-        }
-        for(int i = l;i<=r;++i)
-            v[i]=me[i-l];
-            
-        return;
-    }
-    void sort(vector<int>&v,int l,int r)
-    {
-        if(l<r)
-        {
-            int m = (l+r)/2;
-            sort(v,l,m);
-            sort(v,m+1,r);
-            mer(v,l,m,r);
-        }
-        return;
-    }
     void sortColors(vector<int>& nums) {
-     
-        sort(nums,0,nums.size()-1);
-    
+        map<int,int>m;
+        for(auto c:nums)
+            m[c]++;
+        int j=0;
+        for(auto c:m)
+        {
+            for(int i=0;i<c.second;++i)
+                nums[j++]=c.first;
+        }
     }
 };
