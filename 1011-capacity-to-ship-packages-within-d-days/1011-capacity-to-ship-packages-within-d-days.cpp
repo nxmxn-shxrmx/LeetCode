c51b1b7c1afae4b70678e17d7f18002623b70400
Naman Sharma <82465837+nxmxn-shxrmx@users.noreply.github.com>
2022-09-06 03:17:04 +0530

    Time: 158 ms (5.04%), Space: 45.5 MB (8.32%) - LeetHub

---
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+      bool vali(int x,vector<int>v,int k)
+    {
+        for(int i=0;i<v.size();)
+        {
+            if(k==0)
+                return false;
+            int sum=0;
+            while(i<v.size() && sum+v[i]<=x)
+                sum+=v[i++];
+            k--;
+        }
+        return true;
+    }
+    int shipWithinDays(vector<int>& nums, int m) {
+         int r=0;
+        int l=0;
+        for(auto c:nums)
+        {
+            r+=c;
+            l=max(c,l);
+        }
+        int ans=0;
+        while(l<=r)
+        {
+            int mid=(l+r)/2;
+            if(vali(mid,nums,m))
+                ans=mid,r=mid-1;
+            else
+                l=mid+1;
+        }
+        return ans;
+    }
+};
+
+  
+    
