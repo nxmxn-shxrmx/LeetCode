c487ae2c5a6997855e1f29817ac2c1a48b47de03
Naman Sharma <82465837+nxmxn-shxrmx@users.noreply.github.com>
2022-09-17 18:07:28 +0530

    Time: 15 ms (73.11%), Space: 9.9 MB (73.21%) - LeetHub

--- 959-regions-cut-by-slashes/959-regions-cut-by-slashes.cpp
+++ 959-regions-cut-by-slashes/959-regions-cut-by-slashes.cpp
@@ -1,93 +1,86 @@
 class Solution {
 public:
-    map<int,int>m;
-    map<int,long long>s;
-    int find(int v)
-    {
-        if(m[v]==v)
-            return v;
-        return m[v]=find(m[v]);
-    }
+    
+    vector<int>m;
+    vector<long long>s;
     void make(int a,int b)
     {
-        if(m.find(a)==m.end())
+        if(m[a]==-1)
             m[a]=a,s[a]=1;
-        if(m.find(b)==m.end())
+        if(m[b]==-1)
             m[b]=b,s[b]=1;
+        int u = par(a);
+        int v = par(b);
         
-        int u =find(a);
-        int v =find(b);
-        
-        if(s[v]>s[u])
-            swap(v,u);
-        
-        m[v]=u;
-        s[u]+=s[v];
+        if(u!=v)
+        {
+            if(s[v]>s[u])
+                swap(v,u);
+            m[v]=u;
+            s[u]+=s[v];
+        }
     }
-    
+    int par(int i)
+    {
+        if(i==m[i])
+            return i;
+        return m[i]=par(m[i]);
+    }
     int regionsBySlashes(vector<string>& g) {
-     
-        if(g.size()==0)
-            return 0;
-        int n=g.size();
-        for(int i=1;i<=n;++i)
+        m = vector<int>((g.size()+1)*(g.size()+1),-1);
+        s = vector<long long>((g.size()+1)*(g.size()+1));
+        int n = g.size();
+    for(int i=1;i<=n;++i)
         {  
             make(i-1,i);
             make((i-1)*(n+1),i*(n+1));
             make(n*(n+1)+i-1,n*(n+1)+i);
             make((i-1)*(n+1)+n,i*(n+1)+n);
         }
-        int c=0;
-        for(int i=0;i<n;++i)
+        n++;
+        int k= 0;
+        for(int i = 0;i<g.size();++i)
         {
-            for(int j=0;j<n;++j)
+            for(int j = 0;j<g[0].size();++j)
             {
-                if(g[i][j]==' ')
-                    continue;
                 if(g[i][j]=='/')
                 {
-                    int co = i*(n+1)+(j+1);
-                    int ro = (i+1)*(n+1)+j;
+                    int y = i*n+(j+1);
+                    int x = (i+1)*(n)+j;
+                    if(m[x]==-1)
+                        m[x]=x,s[x]=1;
+                    if(m[y]==-1)
+                        m[y]=y,s[y]=1;
                     
-                    if(m.find(co)==m.end())
-                    {
-                        m[co]=co;
-                        s[co]=1;
-                    }
-                    if(m.find(ro)==m.end())
-                    {
-                        m[ro]=ro;
-                        s[ro]=1;
-                    }
-                    int u = find(co);
-                    int v = find(ro);
+                    int u =par(x);
+                    int v =par(y);
                     if(m[u]==m[v])
-                        c++;
-                    make(ro,co);
+                        k++;
+                    
+                        make(u,v);
+                    
                 }
-                else
+                if(g[i][j]=='\\')
                 {
-                    int co = i*(n+1)+j;
-                    int ro = (i+1)*(n+1)+j+1;
-                       if(m.find(co)==m.end())
-                    {
-                        m[co]=co;
-                        s[co]=1;
-                    }
-                    if(m.find(ro)==m.end())
-                    {
-                        m[ro]=ro;
-                        s[ro]=1;
-                    }
-                    int u = find(co);
-                    int v = find(ro);
+                    int y = i*(n)+j;
+                    int x = (i+1)*(n)+j+1;
+                    if(m[x]==-1)
+                        m[x]=x,s[x]=1;
+                    if(m[y]==-1)
+                        m[y]=y,s[y]=1;
+                    
+                    int u =par(x);
+                    int v =par(y);
                     if(m[u]==m[v])
-                        c++;
-                    make(ro,co);
+                        k++;
+                    
+                        make(u,v);
+                    
                 }
             }
         }
         
-        return c+1;   
+        return k+1;
     }
 };
+
