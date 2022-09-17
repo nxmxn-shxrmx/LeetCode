412f5d2fdb0c78bb28e1cbbc55919d8567d203c1
Naman Sharma <82465837+nxmxn-shxrmx@users.noreply.github.com>
2022-09-17 18:52:23 +0530

    Time: 17 ms (20.18%), Space: 11.6 MB (28.96%) - LeetHub

--- 990-satisfiability-of-equality-equations/990-satisfiability-of-equality-equations.cpp
+++ 990-satisfiability-of-equality-equations/990-satisfiability-of-equality-equations.cpp
@@ -1,107 +1,97 @@
 class Solution {
 public:
     map<char,char>m;
-    map<char,long long>size;
-    char find(char v)
+    map<char,long long>s;
+    
+    char par(char c)
     {
-        if(m[v]==v)
-            return v;
-        
-        return m[v]=find(m[v]);
+        if(m[c]==c)
+            return c;
+        return m[c]=par(m[c]);
     }
-    void make(char a,char b)
-    {
+    bool equationsPossible(vector<string>& e) {
         
-        if(m.find(a)==m.end() && m.find(b)==m.end())
-        {
-            if(a==b)
-                return;
-            m[a]=a;
-            m[b]=a;
-            size[a]+=2;
-        }
-        else if(m.find(a)==m.end())
-        {
-            char u = find(b);
-            m[a]=u;
-            size[u]+=1;
-        }
-        else if(m.find(b)==m.end())
-        {
-            char u = find(a);
-            m[b]=u;
-            size[u]+=1;
-        }
-        else
-        {
-            char u = find(a);
-            char v = find(b);
-            if(size[u]<size[v])
-                swap(u,v);
-            
-            m[v]=u;
-            size[u]+=size[v];
-        }
-    }
-    bool equationsPossible(vector<string>& eq) {
-        
-        for(auto c:eq)
+        for(auto c:e)
         {
             char a = c[0];
             char b = c[3];
-        
-            if(c[1]=='!')
+            
+            if(c[1]=='=')
             {
-                if(a==b)
-                    return false;
                 if(m.find(a)==m.end() && m.find(b)==m.end())
                 {
                     m[a]=a;
-                    m[b]=b;
-                    
-                    size[a]=1;
-                    size[b]=1;
+                    s[a]+=2;
+                    m[b]=a;
                 }
                 else if(m.find(a)==m.end())
                 {
+                    char x=par(b);
+                    m[a]=x;
+                    s[x]+=1;
+                }
+                else if(m.find(b)==m.end())
+                {
+                    char x=par(a);
+                    m[b]=x;
+                    s[x]+=1;
+                }
+                else
+                {
+                    char x =par(a);
+                    char y = par(b);
+                    if(x==y)
+                        continue;
+                    if(s[y]>s[x])
+                        swap(x,y);
+                    m[y]=x;
+                    s[x]+=s[y];
+                }
+            }
+            
+            else
+            {
+                if(a==b)
+                    return false;
+                 if(m.find(a)==m.end() && m.find(b)==m.end())
+                {
                     m[a]=a;
-                    size[a]+=1;
+                    s[a]=1;
+                    s[b]=1;
+                    m[b]=b;
+                }
+                else if(m.find(a)==m.end())
+                {
+                    char x=par(b);
+                    m[a]=a;
+                    s[a]+=1;
                 }
                 else if(m.find(b)==m.end())
                 {
                     m[b]=b;
-                    size[b]+=1;
+                    s[b]=1;
                 }
                 else
                 {
-                    char u = find(a);
-                    char v = find(b);
-                    if(u==v)
+                    char x =par(a);
+                    char y = par(b);
+                    if(x==y)
                         return false;
                 }
             }
-            else
-                make(a,b);
         }
-        
-        
-        for(auto c:eq)
+        for(auto c:e)
         {
-            char a= c[0];
-            char b = c[3];
-            char u = find(a);
-            char v = find(b);
-            
+            char a = par(c[0]);
+            char b = par(c[3]);
+        
+            if(c[1]=='=')
+                if(a!=b)
+                return false;
             if(c[1]=='!')
-            {
-                if(u==v)
-                    return false;
-            }
-            else
-            {
-                if(u!=v)
-                    return false;
-            }
+                if(a==b)
+                return false;
+                
         }
         return true;
     }
