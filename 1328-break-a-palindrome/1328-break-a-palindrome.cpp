96ece8b024867e4b9aab4dacca7737fade9c0fc5
Naman Sharma <82465837+nxmxn-shxrmx@users.noreply.github.com>
2022-10-10 17:07:16 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (45.53%) - LeetHub

---
+++ 1328-break-a-palindrome/1328-break-a-palindrome.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    string breakPalindrome(string s) {
+        
+        if(s.size()==0 || s.size()==1)
+            return "";
+    
+        int j = s.size()-1;
+        for(int i = 0;i<s.size()/2;++i)
+        {
+            if('a'!=s[j])
+            {
+                s[i]='a';
+                return s;
+            }
+            --j;
+        }
+        
+        j =s.size()-1;
+        for(int i = 0;i<s.size()/2;++i)
+        {
+            if('b'!=s[i])
+            {
+                s[j]='b';
+                return s;
+            }
+            --j;
+            
+        }
+        return s;
+    }
+};
+
