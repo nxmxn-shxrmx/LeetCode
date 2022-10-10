2e145f9d4fb8e8edf5417efc9e8f8406de708653
Naman Sharma <82465837+nxmxn-shxrmx@users.noreply.github.com>
2022-10-10 17:09:23 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (78.51%) - LeetHub

--- 1328-break-a-palindrome/1328-break-a-palindrome.cpp
+++ 1328-break-a-palindrome/1328-break-a-palindrome.cpp
@@ -17,16 +17,7 @@ public:
         }
         
         j =s.size()-1;
-        for(int i = 0;i<s.size()/2;++i)
-        {
-            if('b'!=s[i])
-            {
-                s[j]='b';
-                return s;
-            }
-            --j;
-            
-        }
+        s[j]='b';
         return s;
     }
 };
