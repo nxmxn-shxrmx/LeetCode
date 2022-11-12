dd765d83890d6a0559aed39490156cbca9082fc8
Naman Sharma <82465837+nxmxn-shxrmx@users.noreply.github.com>
2022-11-13 02:09:48 +0530

    Time: 19 ms (69.63%), Space: 9 MB (60.43%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -20,7 +20,24 @@ public:
         return v[i][j]=k;
     }
     int minDistance(string s, string t) {
-        v = vector<vector<int>>(s.size(),vector<int>(t.size(),-1));
-        return f(s,t,0,0);
+        v = vector<vector<int>>(s.size()+1,vector<int>(t.size()+1,0));
+        for(int i = 0;i<=t.size();++i)
+            v[s.size()][i]=t.size()-i;
+        for(int i = 0;i<=s.size();++i)
+            v[i][t.size()]=s.size()-i;
+        
+        for(int i = s.size()-1;~i;--i)
+        {
+            for(int j = t.size()-1;~j;--j)
+            {
+                int k = v[i+1][j+1];
+                if(s[i]!=t[j])
+                    k+=1;
+                k =  min(v[i+1][j]+1,k);
+                k = min(v[i][j+1]+1,k);
+                v[i][j]=k;
+            }
+        }
+        return v[0][0];
     }
 };
