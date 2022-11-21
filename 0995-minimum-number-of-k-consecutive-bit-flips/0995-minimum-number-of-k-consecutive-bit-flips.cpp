e5cf37e6cdd86bf97b63c072125fd08189d40ed9
Naman Sharma <82465837+nxmxn-shxrmx@users.noreply.github.com>
2022-11-22 05:04:37 +0530

    Time: 364 ms (18.83%), Space: 106 MB (100.00%) - LeetHub

--- 0995-minimum-number-of-k-consecutive-bit-flips/0995-minimum-number-of-k-consecutive-bit-flips.cpp
+++ 0995-minimum-number-of-k-consecutive-bit-flips/0995-minimum-number-of-k-consecutive-bit-flips.cpp
@@ -2,17 +2,16 @@ class Solution {
 public:
     int minKBitFlips(vector<int>& nums, int k) {
         int b = 0;
-        queue<int>q;
         int c = 0;
         for(int i = 0;i<nums.size();++i)
         {
-            if(!q.empty()&&q.front()==i)
-                b = !b,q.pop();
+            if(i-k>=0 && nums[i-k]==-1)
+                b = !b;
             
             if(b==nums[i])
             {
                 b = !b;
-                q.push(i+k);
+                nums[i]=-1;
                 c++;
             
                 if(k+i>nums.size())
