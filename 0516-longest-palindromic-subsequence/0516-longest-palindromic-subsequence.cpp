7133c26807de193f881f7ceefce0c427150e9c90
Naman Sharma <82465837+nxmxn-shxrmx@users.noreply.github.com>
2022-11-22 05:41:43 +0530

    Time: 77 ms (94.65%), Space: 6.6 MB (95.13%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
@@ -1,23 +1,25 @@
 class Solution {
 public:
-    int dp[3001][3001];
-    int f(int i,int j,string s)
-    {
-        if(i>j)
-            return 0;
-        if(i==j)
-            return 1;
-        if(dp[i][j]!=-1)
-            return dp[i][j];
+//     int dp[3001][3001];
+//     int f(int i,int j,string s)
+//     {
+//         if(i>j)
+//             return 0;
+//         if(i==j)
+//             return 1;
+//         if(dp[i][j]!=-1)
+//             return dp[i][j];
         
-        if(s[i]==s[j])
-            return dp[i][j]=f(i+1,j-1,s)+2;
-        return dp[i][j] = max(f(i+1,j,s),f(i,j-1,s));
+//         if(s[i]==s[j])
+//             return dp[i][j]=f(i+1,j-1,s)+2;
+//         return dp[i][j] = max(f(i+1,j,s),f(i,j-1,s));
             
-    }
+//     }
     int longestPalindromeSubseq(string s) {
       
         int n = s.size();
+        vector<int>cur(n+1);
+        vector<int>pre(n+1);
         for(int i = s.size()-1;~i;--i)
         {
             for(int j = 1;j<=s.size();++j)
@@ -25,11 +27,12 @@ public:
                 if(i>j)
                     continue;
                 if(i==j)
-                    dp[i][j]=1;
-               else if(s[n-i-1]==s[n-j]) dp[i][j]=dp[i+1][j-1]+2;
+                    cur[j]=1;
+               else if(s[n-i-1]==s[n-j]) cur[j]=pre[j-1]+2;
                 
-                else dp[i][j] = max(dp[i+1][j],dp[i][j-1]);   
+                else cur[j] = max(pre[j],cur[j-1]);   
             }
+            pre= cur;
         }
         // for(int i = 0;i<=s.size();++i)
         // {
@@ -37,6 +40,6 @@ public:
         //         cout<<dp[i][j]<<" ";
         //     cout<<"\n";
         // }
-        return dp[0][s.size()]-1;
+        return cur[s.size()]-1;
     }
 };
