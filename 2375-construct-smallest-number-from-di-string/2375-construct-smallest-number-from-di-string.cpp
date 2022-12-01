add022cf56d8850172af2a088383e88fcb4e0bbe
Naman Sharma <82465837+nxmxn-shxrmx@users.noreply.github.com>
2022-12-01 23:50:18 +0530

    Time: 834 ms (5.06%), Space: 74.2 MB (5.06%) - LeetHub

---
+++ 2375-construct-smallest-number-from-di-string/2375-construct-smallest-number-from-di-string.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    set<string>s;
+    void f(int i,string t,string p,vector<int>v)
+    {
+        if(i==p.size())
+        {
+            s.insert(t);
+            return;
+        }
+        
+        if(p[i]=='I')
+        {
+            for(int j = t[t.size()-1]-'0' +1 ;j<=9;++j)
+            {
+                if(!v[j])
+                {
+                    v[j]=1;
+                    f(i+1,t+to_string(j),p,v);
+                    v[j]=0;
+                }
+            }
+        }
+        else
+        {
+            for(int j = t[t.size()-1]-'0'-1;j>=1;--j)
+            {
+                if(!v[j])
+                {
+                    v[j]=1;
+                    f(i+1,t+to_string(j),p,v);
+                    v[j]=0;
+                }
+            }
+        }
+    }
+    string smallestNumber(string pattern) {
+        vector<int>v(10);
+        for(int i = 1;i<10;++i)
+        {
+            v[i]=1;
+            f(0,""+to_string(i),pattern,v);
+            v[i]=0;
+        }
+        return *s.begin();
+    }
+};
