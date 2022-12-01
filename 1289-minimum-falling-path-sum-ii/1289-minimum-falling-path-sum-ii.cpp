ed1b891d480f6a705ef60c2e9ad1479e47337a09
Naman Sharma <82465837+nxmxn-shxrmx@users.noreply.github.com>
2022-12-02 01:23:36 +0530

    Time: 50 ms (95.84%), Space: 14.7 MB (74.40%) - LeetHub

--- 1289-minimum-falling-path-sum-ii/1289-minimum-falling-path-sum-ii.cpp
+++ 1289-minimum-falling-path-sum-ii/1289-minimum-falling-path-sum-ii.cpp
@@ -1,23 +1,58 @@
 class Solution {
 public:
     int minFallingPathSum(vector<vector<int>>&m) {
-         int ans = m[0][0];
+       
+        vector<pair<int,int>>v(2,{INT_MAX,0});
+        for(int i = 0;i<m.size();++i)
+        {
+            if(m[0][i]<=v[0].first)
+            {
+                v[1]=v[0];
+                v[0].first=m[0][i];
+                v[0].second=i;
+            }
+            else if(m[0][i]<v[1].first)
+            {
+                v[1].first=m[0][i];
+                v[1].second=i;
+            }
+        }
+    
         for(int i = 1;i<m.size();++i)
         {
-            ans = INT_MAX;
+            vector<pair<int,int>>q(2,{INT_MAX,0});
             for(int j=0;j<m.size();++j)
             {
                 int p = INT_MAX;
-                int k = j-1;
-                while(k>=0)
-                    p = min(p,m[i-1][k]),k--;
-                k =j+1;
-                while(k<m.size())
-                    p = min(p,m[i-1][k]),k++;
+                if(v[0].second!=j)
+                    p = v[0].first;
+                else
+                    p = v[1].first;
                 m[i][j]+=p;
-                ans= min(m[i][j],ans);
+
+                if(m[i][j]<=q[0].first)
+                {
+                q[1]=q[0];
+                    q[0].first=m[i][j];
+                    q[0].second=j;
+                }
+                else if(m[i][j]<=q[1].first)
+                {
+                    q[1].first=m[i][j];
+                    q[1].second=j;
+                }
+             
             }
+              v[0]=q[0];
+            v[1]=q[1];
+            
         }
-        return  ans;
+        // for(auto c:m)
+        // {
+        //     for(auto x:c)
+        //         cout<<x<<" ";
+        //     cout<<"\n";
+        // }
+        return  v[0].first;
     }
 };
