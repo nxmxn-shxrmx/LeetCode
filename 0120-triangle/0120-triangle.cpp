2fef8ba6f504b0d833da1f00c5646d08d57d7cc1
Naman Sharma <82465837+nxmxn-shxrmx@users.noreply.github.com>
2022-12-02 00:47:25 +0530

    Time: 7 ms (88.47%), Space: 8.7 MB (78.43%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -2,18 +2,18 @@ class Solution {
 public:
   
     int minimumTotal(vector<vector<int>>& t) {
-            
+             int ans = INT_MAX;
+        ans= t[0][0];
         for(int i = 1;i<t.size();++i)
-        {
+        {  
             t[i][0]+=t[i-1][0];
+            ans = t[i][0];
             for(int j =1;j<t[i-1].size();++j)
-                t[i][j]=min(t[i-1][j-1],t[i-1][j])+t[i][j];
+                t[i][j]=min(t[i-1][j-1],t[i-1][j])+t[i][j],ans=min(ans,t[i][j]);
             t[i][t[i-1].size()]+=t[i-1][t[i-1].size()-1];
+            ans = min(ans,t[i][t[i-1].size()]);
         }
-        int ans = INT_MAX;
-     
-        for(auto c:t[t.size()-1])
-            ans = min(ans,c);
+        
         return ans;
     }
 };
