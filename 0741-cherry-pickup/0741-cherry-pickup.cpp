be700e778f5c074b840da1b7933f43178cdcb350
Naman Sharma <82465837+nxmxn-shxrmx@users.noreply.github.com>
2022-12-22 23:53:17 +0530

    Time: 504 ms (22.43%), Space: 38.8 MB (23.98%) - LeetHub

---
+++ 0741-cherry-pickup/0741-cherry-pickup.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    int dp[51][51][51][51];
+    int e = 0;
+    int f(int i,int j,int a,int b,vector<vector<int>>&g)
+    {
+          if(i>=g[0].size() || j>=g[0].size() || a>=g[0].size() || b>=g[0].size() || g[i][j]==-1 || g[a][b]==-1)
+            return -99999;
+        if( i==g[0].size()-1 && j==g[0].size()-1)
+        {
+            e=1;
+    
+            return g[i][j];
+        }
+        if(dp[i][j][a][b]!=-1)
+            return dp[i][j][a][b];
+        int ans= 0;
+        ans = max(f(i+1,j,a+1,b,g),f(i+1,j,a,b+1,g));
+        ans = max(ans,f(i,j+1,a+1,b,g));
+        ans = max(ans,f(i,j+1,a,b+1,g));
+    
+         ans+=g[i][j];
+        if(a!=i || j!=b)
+            ans+=g[a][b];
+        
+        return dp[i][j][a][b]=ans;
+    }
+    int cherryPickup(vector<vector<int>>& grid) {
+        memset(dp,-1,sizeof(dp));
+    
+        int ans=f(0,0,0,0,grid);
+        if(e)
+            return ans;
+        
+        else
+            return 0;
+    }
+};
