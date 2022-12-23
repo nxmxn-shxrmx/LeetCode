a254f4a7a56fb5ce50ef49d70eb4a87b68e09647
Naman Sharma <82465837+nxmxn-shxrmx@users.noreply.github.com>
2022-12-23 21:47:21 +0530

    Time: 303 ms (76.93%), Space: 75.1 MB (95.39%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    
+   //memorization 
 //     int f(int i,int k,int b,vector<int>&v)
 //     {
 //         if(i==v.size() || k==2)
@@ -14,17 +14,28 @@ public:
 //         return dp[i][b][k]=max(f(i+1,k+1,1,v)+v[i],f(i+1,k,b,v));
 //     }
     int maxProfit(vector<int>& prices) {
-        vector<vector<int>>pre(2,vector<int>(3,0));
-        vector<vector<int>>cur(2,vector<int>(3,0));
-        for(int i = prices.size()-1;~i;--i)
+        //tabulation
+        // vector<vector<int>>pre(2,vector<int>(3,0));
+        // vector<vector<int>>cur(2,vector<int>(3,0));
+        // for(int i = prices.size()-1;~i;--i)
+        // {
+        //     for(int k = 1;~k;--k)
+        //     {
+        //         cur[1][k]=max(pre[0][k]-prices[i],pre[1][k]);
+        //         cur[0][k]=max(pre[1][k+1]+prices[i],pre[0][k]);
+        //     }
+        //     pre= cur;
+        // }
+        // return cur[1][0];
+        int b1 = INT_MAX,b2=INT_MAX;
+        int s1=0,s2=0;
+        for(auto c:prices)
         {
-            for(int k = 1;~k;--k)
-            {
-                cur[1][k]=max(pre[0][k]-prices[i],pre[1][k]);
-                cur[0][k]=max(pre[1][k+1]+prices[i],pre[0][k]);
-            }
-            pre= cur;
+            b1 = min(b1,c);
+            s1 = max(s1,c-b1);
+            b2 = min(b2,c-s1);
+            s2 = max(s2,c-b2);
         }
-        return cur[1][0];
+        return s2;
     }
 };
