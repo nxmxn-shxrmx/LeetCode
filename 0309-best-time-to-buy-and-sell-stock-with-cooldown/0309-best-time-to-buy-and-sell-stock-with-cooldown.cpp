42319a6d0bd78478f1848b45e4649c53227411ab
Naman Sharma <82465837+nxmxn-shxrmx@users.noreply.github.com>
2022-12-23 22:06:42 +0530

    Time: 7 ms (65.82%), Space: 11.3 MB (74.16%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
-    int dp[5001][2];
-    int f(int i,int b,vector<int>&v)
-    {
-        if(i>=v.size())
-            return 0;
-        if(dp[i][b]!=-1)
-            return dp[i][b];
-        if(b)
-            return dp[i][b]= max(f(i+1,0,v)-v[i],f(i+1,b,v));
-        return dp[i][b]= max(f(i+2,1,v)+v[i],f(i+1,b,v));
-    }
+    // int dp[5001][2];
+    // int f(int i,int b,vector<int>&v)
+    // {
+    //     if(i>=v.size())
+    //         return 0;
+    //     if(dp[i][b]!=-1)
+    //         return dp[i][b];
+    //     if(b)
+    //         return dp[i][b]= max(f(i+1,0,v)-v[i],f(i+1,b,v));
+    //     return dp[i][b]= max(f(i+2,1,v)+v[i],f(i+1,b,v));
+    // }
     int maxProfit(vector<int>& prices) {
         vector<int>pre(2,0);
         vector<int>cur(2,0);
