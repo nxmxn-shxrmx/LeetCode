8be04f193228e4e02ccf028ba3e92eedd300f873
Naman Sharma <82465837+nxmxn-shxrmx@users.noreply.github.com>
2022-12-31 14:40:29 +0530

    Time: 6 ms (52.01%), Space: 7 MB (91.48%) - LeetHub

--- 0980-unique-paths-iii/0980-unique-paths-iii.cpp
+++ 0980-unique-paths-iii/0980-unique-paths-iii.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
-    vector<vector<int>>vis;
+  
     
     int f(int i,int j ,vector<vector<int>>&grid,int p)
     {
-        if(i<0 || j<0 || i==grid.size() || j==grid[0].size() || grid[i][j]==-1 || vis[i][j])
+        if(i<0 || j<0 || i==grid.size() || j==grid[0].size() || grid[i][j]==-1)
             return 0;
         if(grid[i][j]==2)
         {
@@ -13,21 +13,23 @@ public:
             
             return 0;
         }
-        vis[i][j]=1;
+    
         if(grid[i][j]==0)
             p--;
+        int t = -1;
+        swap(grid[i][j],t);
         int d[5]={1,0,-1,0,1};
         int k = 0;
         for(int c = 1;c<5;++c)
             k+=f(i+d[c-1],j+d[c],grid,p);
-        vis[i][j]=0;
+        swap(grid[i][j],t);
         return k;
     }
     int uniquePathsIII(vector<vector<int>>& grid) {
         int p = 0;
         int x = 0;
         int y = 0;
-        vis = vector<vector<int>>(grid.size(),vector<int>(grid[0].size()));
+       // vis = vector<vector<int>>(grid.size(),vector<int>(grid[0].size()));
         for(int i = 0;i<grid.size();++i)
         {
             for(int j = 0;j<grid[0].size();++j)
