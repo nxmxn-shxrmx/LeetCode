81e58b7498650d3a88b281ec12b58471d0f9d6e2
Naman Sharma <82465837+nxmxn-shxrmx@users.noreply.github.com>
2023-01-09 19:47:30 +0530

    Time: 33 ms (90.04%), Space: 12.6 MB (63.36%) - LeetHub

--- 0149-max-points-on-a-line/0149-max-points-on-a-line.cpp
+++ 0149-max-points-on-a-line/0149-max-points-on-a-line.cpp
@@ -2,41 +2,21 @@ class Solution {
 public:
     int maxPoints(vector<vector<int>>& points) {
         int m=0;
-        vector<vector<double>>sl(points.size(),vector<double>(points.size()));
         for(int i = 0;i<points.size();++i)
         {
-          
+            map<double,int>mi;
             for(int j = i+1;j<points.size();++j)
             {
                 double p = points[i][1]-points[j][1];
                 double y = points[i][0]-points[j][0];
-                if(p==0)
-                {
-                    sl[i][j]=0;
-                    sl[j][i]=0;
-                }
-                else
-                {
-                    sl[i][j]=p/y;
-                    sl[j][i]=p/y;
-                }
+                double h = p/y;
+                if(y==0)
+                    h = INT_MAX;
+                mi[h]++;
+                m = max(m,mi[h]);
             }
         }
-        for(int i = 0;i<points.size();++i)
-        {
-           map<double,int>mi;
-            int u =0;
-            for(int j = 0;j<points.size();++j)
-            {
-                if(i!=j)
-                {
-                    mi[sl[i][j]]++;
-                    u= max(u,mi[sl[i][j]]);
-                }
-            }
-            m = max(u,m);
-        }
-        
+
         return m+1;
     }
 };
