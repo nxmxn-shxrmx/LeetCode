d3f7c1a2e5079e32315c1a8f31eae575cbafbd26
Naman Sharma <82465837+nxmxn-shxrmx@users.noreply.github.com>
2023-01-12 14:05:15 +0530

    Time: 679 ms (74.08%), Space: 232 MB (29.63%) - LeetHub

---
+++ 1519-number-of-nodes-in-the-sub-tree-with-the-same-label/1519-number-of-nodes-in-the-sub-tree-with-the-same-label.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    vector<int>ans;
+    vector<int>f(int i,int pre,vector<vector<int>>&v,string &l)
+    {
+        vector<int>u(26);
+        for(auto c:v[i])
+        {
+            if(c==pre)
+                continue;
+            
+            vector<int>p=f(c,i,v,l);
+            for(int i = 0;i<p.size();++i)
+                u[i]+=p[i];
+            
+        }
+        u[l[i]-'a']++;
+        ans[i] = u[l[i]-'a'];
+        return u;
+    }
+    vector<int> countSubTrees(int n, vector<vector<int>>& edges, string labels) {
+        vector<vector<int>>v(n);
+        ans = vector<int>(n);
+        for(auto c:edges)
+        {
+            v[c[0]].push_back(c[1]);
+            v[c[1]].push_back(c[0]);
+        }
+        f(0,-1,v,labels);
+        return ans;
+    }
+};
