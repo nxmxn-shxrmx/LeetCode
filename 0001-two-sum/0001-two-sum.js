4b76023aedf6990af5e970e7f05cfa06f267b3ff
Naman Sharma <82465837+nxmxn-shxrmx@users.noreply.github.com>
2023-01-16 18:16:56 +0530

    Time: 63 ms (96.94%), Space: 42.3 MB (76.07%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -1,26 +1,11 @@
-/**
- * @param {number[]} nums
- * @param {number} target
- * @return {number[]}
- */
 var twoSum = function(nums, target) {
-    let l = 0;
-    let r =  nums.length-1;
-    var a =[];
-    for(var i = 0;i<nums.length;++i)
-    {
-        a[i] = [nums[i],i];
+    let hash = {};
+    for (let i = 0; i < nums.length; i++) {
+        let complement = target - nums[i];
+        if (hash[complement] !== undefined) {
+            return [hash[complement], i];
+        }
+        hash[nums[i]] = i;
     }
-    a.sort((x,y)=>
-          {
-        return x[0]-y[0];
-    });
-   while(l<r)
-   {
-        if(a[l][0]+a[r][0]==target)
-            return [a[l][1],a[r][1]];
-       else if(a[l][0]+a[r][0]>target) r--;
-       else l++; 
-   }
     return [];
 };
