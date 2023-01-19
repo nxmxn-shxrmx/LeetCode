1c052ab8b63c61928cc36a7dc834b35d02c191d0
Naman Sharma <82465837+nxmxn-shxrmx@users.noreply.github.com>
2023-01-19 18:57:01 +0530

    Time: 3 ms (85.11%), Space: 6.4 MB (78.87%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.cpp
+++ 0097-interleaving-string/0097-interleaving-string.cpp
@@ -3,9 +3,7 @@ public:
     int dp[201][201];
     bool f(string &s1,string &s2,string &s3,int i,int j)
     {
-        if(i+j==s3.size())return 1;
-        
-        
+        if(i+j==s3.size())return 1;        
         if(dp[i][j]!=-1)return dp[i][j];
         dp[i][j]=0;
         if(i<s1.size()&&s3[i+j]==s1[i])
@@ -17,8 +15,19 @@ public:
         return dp[i][j];
     }
     bool isInterleave(string s1, string s2, string s3) {
-        memset(dp,-1,sizeof(dp));
         if(s1.size()+s2.size()!=s3.size())return 0;
-        return f(s1,s2,s3,0,0);
+        dp[s1.size()][s2.size()]=1;
+        for(int i = s3.size()-1;~i;--i)
+        {
+            for(int j = s3.size()-1;~j;--j)
+            {
+                if(i<s1.size()&&(i+j)<s3.size()&&s1[i]==s3[i+j])
+                    dp[i][j]=dp[i+1][j];
+                if(j<s2.size()&&(i+j)<s3.size()&&s2[j]==s3[i+j])
+                    dp[i][j]|=dp[i][j+1];
+                
+            }
+        }
+        return dp[0][0];
     }
 };
