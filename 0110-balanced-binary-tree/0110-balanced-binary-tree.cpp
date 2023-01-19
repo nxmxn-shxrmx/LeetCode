e5b597a4df0420bf08a5b3d0459f466fd853e06d
Naman Sharma <82465837+nxmxn-shxrmx@users.noreply.github.com>
2023-01-19 14:57:38 +0530

    Time: 14 ms (72.31%), Space: 20.9 MB (59.59%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
@@ -11,20 +11,23 @@
  */
 class Solution {
 public:
-    bool ans = 1;
+    
     int h(TreeNode* r)
     {
         if(r==NULL)
             return 0;
         
         int h1 = h(r->left);
+        if(h1==-1)
+            return -1;
         int h2 = h(r->right);
+        if(h2==-1)
+            return -1;
         if(abs(h1-h2)>=2)
-            ans = 0;
+            return -1;
         return max(h1,h2)+1;
     }
     bool isBalanced(TreeNode* root) {
-        h(root);
-        return ans;
+        return h(root)!=-1;
     }
 };
