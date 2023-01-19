1275600db22b0d1ece88fe7e7a84d2af63b77b63
Naman Sharma <82465837+nxmxn-shxrmx@users.noreply.github.com>
2023-01-19 14:52:14 +0530

    Time: 25 ms (32.61%), Space: 21 MB (59.59%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
@@ -11,26 +11,20 @@
  */
 class Solution {
 public:
-    unordered_map<TreeNode*,int>m; 
+    bool ans = 1;
     int h(TreeNode* r)
     {
         if(r==NULL)
             return 0;
-        if(m.find(r)!=m.end())
-            return m[r];
-        int h1 =h(r->left);
-        int h2 =h(r->right);
-        return m[r]=max(h1,h2)+1;
+        
+        int h1 = h(r->left);
+        int h2 = h(r->right);
+        if(abs(h1-h2)>=2)
+            ans = 0;
+        return max(h1,h2)+1;
     }
     bool isBalanced(TreeNode* root) {
-        if(root==NULL)
-            return 1;
-        if(!isBalanced(root->left) || !isBalanced(root->right))
-            return 0;
-        int h1 = h(root->left);
-        int h2 = h(root->right);
-        if(abs(h1-h2)>=2)
-            return 0;
-        return 1;
+        h(root);
+        return ans;
     }
 };
