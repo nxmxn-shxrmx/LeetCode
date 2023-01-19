fa577830397b24fb50d137decdc1f6c167d0a72b
Naman Sharma <82465837+nxmxn-shxrmx@users.noreply.github.com>
2023-01-19 18:57:22 +0530

    Time: 7 ms (54.21%), Space: 6.5 MB (75.63%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.cpp
+++ 0097-interleaving-string/0097-interleaving-string.cpp
@@ -1,19 +1,6 @@
 class Solution {
 public:
     int dp[201][201];
-    bool f(string &s1,string &s2,string &s3,int i,int j)
-    {
-        if(i+j==s3.size())return 1;        
-        if(dp[i][j]!=-1)return dp[i][j];
-        dp[i][j]=0;
-        if(i<s1.size()&&s3[i+j]==s1[i])
-            dp[i][j]=f(s1,s2,s3,i+1,j);
-        if(dp[i][j])
-            return 1;
-        if(j<s2.size()&&s3[i+j]==s2[j])
-            dp[i][j]=f(s1,s2,s3,i,j+1);
-        return dp[i][j];
-    }
     bool isInterleave(string s1, string s2, string s3) {
         if(s1.size()+s2.size()!=s3.size())return 0;
         dp[s1.size()][s2.size()]=1;
