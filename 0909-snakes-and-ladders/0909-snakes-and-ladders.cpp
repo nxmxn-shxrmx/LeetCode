de493f7f856dc79ff2c5f756fe67e7a44b92b831
Naman Sharma <82465837+nxmxn-shxrmx@users.noreply.github.com>
2023-01-24 19:35:05 +0530

    Time: 25 ms (67.60%), Space: 13.5 MB (43.99%) - LeetHub

--- 0909-snakes-and-ladders/0909-snakes-and-ladders.cpp
+++ 0909-snakes-and-ladders/0909-snakes-and-ladders.cpp
@@ -1,45 +1,35 @@
 class Solution {
 public:
-    int dp[500][500];
-    int ch[500];
-    int f(int n,int i,int pre,vector<vector<int>>&b)
-    {
-         if(ch[i])return 1000;
-        
-        if(i==((n*n)-1))return dp[i][pre]=0;
-        if(dp[i][pre]!=-1)return dp[i][pre];
-        int ans = 1000;
-        ch[i]=1;
-        for(int k = min(i+6,n*n-1);k>=(i+1);--k)
-        {
-            
-              int r = k/n;
-              int c = k%n;
-              int u = k;
-              if(b[r][c]!=-1)u = b[r][c]-1;
-              
-            ans = min(ans,f(n,u,pre+1,b)+1);
-                
-        }
-         ch[i]=0;
-        return dp[i][pre]=ans;
-    }
-    int snakesAndLadders(vector<vector<int>>& board) {
-        memset(dp,-1,sizeof(dp));
+    int snakesAndLadders(vector<vector<int>>& b) {
         vector<vector<int>>v;
-        int p = 0;
-        for(int i= board.size()-1;~i;--i)
+        bool p = 0;
+        for(int i =b.size()-1;~i;--i)
         {
-            if(p)
-                reverse(board[i].begin(),board[i].end());
-            v.push_back(board[i]);
-            p = !p;
+            if(p)reverse(b[i].begin(),b[i].end());
+            v.push_back(b[i]);
+            p =!p;
         }
-       
-        
-        int u=f(board.size(),0,0,v);
-        if(u>=1000)u=-1;
-     
-        return u;
+        int n =b.size();
+        vector<int>u(n*n,INT_MAX);
+        u[0]=0;
+        queue<int>q;
+        q.push(0);
+        while(!q.empty())
+        {
+            int i = q.front();
+            q.pop();
+            for(int k = i+1;k<=min(i+6,n*n-1);++k)
+            {
+                int p = k;
+                int r = k/n;
+                int c = k%n;
+                if(v[r][c]!=-1)
+                    p =v[r][c]-1;
+                
+                if(u[p]>(u[i]+1))
+                    u[p]=u[i]+1,q.push(p);
+            }
+        }
+        return u[n*n-1]==INT_MAX?-1:u[n*n-1];
     }
 };
