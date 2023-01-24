99dadc4dce039ec6773f3047745e4cfca6c0479a
Naman Sharma <82465837+nxmxn-shxrmx@users.noreply.github.com>
2023-01-24 20:15:16 +0530

    Time: 329 ms (54.79%), Space: 11.3 MB (80.16%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,12 +1,6 @@
 class Solution {
 public:
-    bool f(int i,int t,vector<int>&v)
-    {
-        if(t==0)return 1;
-        if(i==v.size())return 0;
-        
-        return f(i+1,t,v)||f(i+1,t-v[i],v);
-    }
+  
     bool canPartition(vector<int>& nums) {
         int u  =0;
         for(auto c:nums)
@@ -14,18 +8,19 @@ public:
         if(u%2)return 0;
         
         u/=2;
-        vector<vector<int>>dp(nums.size()+1,vector<int>(u+1,0));
-        for(int i = 1;i<=nums.size();++i)
-                dp[i][0]=1;
+        vector<int>pre(u+1,0);
+        vector<int>cur(u+1,0);
+        pre[0]=1;
         for(int i = 1;i<=nums.size();++i)
         {
             for(int j =1;j<=u;++j)
             {
                 if(j-nums[i-1]>=0)
-                    dp[i][j]|=dp[i-1][j-nums[i-1]];
-                dp[i][j]|=dp[i-1][j];
+                    cur[j]|=pre[j-nums[i-1]];
+                cur[j]|=pre[j];
             }
+            pre=cur;
         }
-        return dp[nums.size()][u];
+        return cur[u];
     }
 };
