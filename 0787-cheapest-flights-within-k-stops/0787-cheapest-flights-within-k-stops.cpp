01bbfcc12b5bcca540124d849d8a8bd152c34c41
Naman Sharma <82465837+nxmxn-shxrmx@users.noreply.github.com>
2023-01-26 16:19:10 +0530

    Time: 57 ms (26.86%), Space: 13.9 MB (31.35%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
@@ -1,37 +1,27 @@
 class Solution {
 public:
     int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
-        vector<int>b(n,INT_MAX);
-        vector<vector<pair<int,int>>>v(n);
+        vector<int>dist(n,INT_MAX);
+        priority_queue<vector<int>>q;
+        q.push({0,0,src});
+        dist[src]=0;
+        vector<vector<pair<int,int>>>g(n);
         for(auto c:flights)
-            v[c[0]].push_back({c[1],c[2]});
-        
-        priority_queue<vector<int>,vector<vector<int>>, greater<vector<int>>>q;
-        q.push({0,src,0});
-        
-        b[src]=0;
-        int ans = INT_MAX;
+            g[c[0]].push_back({c[1],c[2]});
         while(!q.empty())
         {
-                int a =  q.top()[2];
-                int u = q.top()[1];
-                int p = q.top()[0];
-                q.pop();
-        
-                if(p>k)
-                    continue;
-    
-                for(auto c:v[u])
-                {
-                    if(b[c.first]>a+c.second)
-                    {
-                        b[c.first]=a+c.second;
-                        q.push({p+1,c.first,a+c.second});
-                    }
-                }
-                
+            int i = q.top()[2];
+            int u = abs(q.top()[0]);
+            int pr =abs(q.top()[1]);
+            q.pop();
+            if(u>k)continue;
+            for(auto c:g[i])
+            {
+                if(dist[c.first]>(pr+c.second))
+                    q.push({-(u+1),-(pr+c.second),c.first}),dist[c.first]=pr+c.second;
+            }
             
         }
-        return b[dst]==INT_MAX?-1:b[dst];
+        return dist[dst]==INT_MAX?-1:dist[dst];
     }
 };
