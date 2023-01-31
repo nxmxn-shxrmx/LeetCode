02db1384192e169694807a100390a18c446a5e53
Naman Sharma <82465837+nxmxn-shxrmx@users.noreply.github.com>
2023-02-01 04:07:05 +0530

    Time: 705 ms (9.07%), Space: 23.8 MB (32.04%) - LeetHub

--- 1626-best-team-with-no-conflicts/1626-best-team-with-no-conflicts.cpp
+++ 1626-best-team-with-no-conflicts/1626-best-team-with-no-conflicts.cpp
@@ -1,32 +1,26 @@
 class Solution {
 public:
-    int dp[1002][1002];
-    int f(int i,vector<vector<int>>&v,int age)
-    {
-        if(i==v.size())return 0;
-        if(dp[i][age]!=-1)return dp[i][age];
-        int u =0;
-        if(age<=v[i][1])
-            u = f(i+1,v,max(v[i][1],age))+v[i][0];
-        return dp[i][age]=max(f(i+1,v,age),u);
-    }
+   
     int bestTeamScore(vector<int>& scores, vector<int>& ages) {
         vector<vector<int>>v;
         int mx =0;
         for(int i = 0;i<scores.size();++i)
             v.push_back({scores[i],ages[i]}),mx = max(v[i][1],mx);
         sort(v.begin(),v.end());
+        vector<int>pre(mx+1);
+        vector<int>cur(mx+1);
         for(int i=v.size()-1;~i;--i)
         {
             for(int j =mx;~j;--j)
             {
                  int u =0;
                 if(j<=v[i][1])
-                u = dp[i+1][max(v[i][1],j)]+v[i][0];
-                 dp[i][j]=max(dp[i+1][j],u); 
+                u = pre[max(v[i][1],j)]+v[i][0];
+                 cur[j]=max(pre[j],u); 
 
             }
+            pre= cur;
         }
-        return dp[0][0];
+        return cur[0];
     }
 };
