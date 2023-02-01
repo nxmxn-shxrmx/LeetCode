f392a8fd7a6d3fa7f637d7395da1d9f51b35e266
Naman Sharma <82465837+nxmxn-shxrmx@users.noreply.github.com>
2023-02-01 11:03:14 +0530

    Time: 3 ms (83.39%), Space: 6.8 MB (93.93%) - LeetHub

--- 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.cpp
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.cpp
@@ -3,39 +3,31 @@ public:
     string gcdOfStrings(string s1, string s2) {
         int u = __gcd(s1.size(),s2.size());
         if(s1.size()<s2.size())swap(s1,s2);
-
-        for(int i = u;i>=1;--i)
+        bool yes=0;
+            
+        for(int j = 0;j<s1.size();j+=u)
         {
-            string p ="";
-            bool yes=0;
-            if(s2.size()%i==0 && s1.size()%i==0)
+            int o = 0;
+            int x =j;
+            int y =j;
+            if(y>=s2.size())y = 0;
+            while(o<u)
             {
-                for(int j = 0;j<s1.size();j+=i)
+                if(s1[x]==s2[y] && s1[x]==s1[o])
+                    ++x,++y,++o;
+                else
                 {
-                    int o = 0;
-                    int x =j;
-                    int y =j;
-                    if(y>=s2.size())y = 0;
-                    while(o<u)
-                    {
-                        if(s1[x]==s2[y] && s1[x]==s1[o])
-                            ++x,++y,++o;
-                        else
-                        {
-                            yes=1;
-                            break;
-                        }
-                    }
-                    if(yes)
+                    yes=1;
                     break;
                 }
-                if(!yes)
-                {
-            
-                    return s1.substr(0,i);
-                }
             }
+            if(yes)
+            break;
         }
+        if(!yes)
+            return s1.substr(0,u);
+
+        
         return "";
     }
 };
