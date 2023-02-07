2399503d19e0fdd73c0ac038c9b1a5458b09cff0
Naman Sharma <82465837+nxmxn-shxrmx@users.noreply.github.com>
2023-02-07 21:47:32 +0530

    Time: 5 ms (86.51%), Space: 7.2 MB (80.92%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -1,42 +1,27 @@
 class Solution {
 public:
+    bool match(vector<int>&v,vector<int>&u)
+    {
+        for(int i = 0;i<26;++i)
+            if(u[i]!=v[i])return 0;
+        return 1;
+    }
     bool checkInclusion(string s1, string s2) {
+        if(s2.size()<s1.size())return 0;
         vector<int>v(26);
-        for(auto c:s1)
-            v[c-'a']++;
-        unordered_map<int,queue<int>>q;
-        for(int i = 0;i<s2.size();++i)
+        vector<int>u(26);
+        for(int i = 0;i<s1.size();++i)
         {
-            vector<int>u(v.begin(),v.end());
-            q.clear();
-            int p =0;
-            int j = i;
-            while(i<s2.size()&&p<s1.size())
-            {
-                if(v[s2[i]-'a']==0)break;
-                if(u[s2[i]-'a']==0)
-                {
-                     int l = q[s2[i]-'a'].front();
-                     for(j;j<=l&&j<s2.size();++j)
-                     {
-                        p--;
-                        q[s2[j]-'a'].pop();
-                        u[s2[j]-'a']++;
-                     }
-                    j=l+1;
-                }
-                u[s2[i]-'a']--;
-                q[s2[i]-'a'].push(i);
-                ++p;
-                ++i;
-            }
-            // for(auto c:v)cout<<c<<" ";
-            // cout<<"\n";
-            // for(auto c:u)cout<<c<<" ";
-            // cout<<"\n";
-            // cout<<p<<"\n";
-            if(p==s1.size())return 1;
+            v[s1[i]-'a']++;
+            u[s2[i]-'a']++;
         }
-        return 0;
+        for(int i = 0;i<s2.size()-s1.size();++i)
+        {
+           if(match(u,v))return 1;
+            u[s2[i+s1.size()]-'a']++;
+            u[s2[i]-'a']--;
+         
+        }
+        return match(u,v);
     }
 };
