966559bfdafc83c536d0831f5e9856d3bb8009c1
Naman Sharma <82465837+nxmxn-shxrmx@users.noreply.github.com>
2023-02-07 16:59:10 +0530

    Time: 122 ms (94.17%), Space: 66.2 MB (89.54%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
@@ -1,29 +1,24 @@
 class Solution {
 public:
     int totalFruit(vector<int>& fruits) {
-        int first=0;
-        int second=0;
-        int first_el=-1;
-        int second_el=-1;
+        int first=-1;
+        int second=-1;
         int ct = 0;
         int ans = 0;
         for(int i = 0;i<fruits.size();++i)
         {
-            if(first_el==-1)first_el=fruits[i],first=i;
-            else if(second_el==-1 && fruits[i]!=first_el)second_el = fruits[i],second = i;
-            else if(first_el!=fruits[i]&&second_el!=fruits[i])
+            if(first==-1)first=i;
+            else if(second==-1 && fruits[i]!=fruits[first])second = i;
+            else if(fruits[first]!=fruits[i]&&fruits[second]!=fruits[i])
             {
                 if(first>=second)
-                {
                     swap(first,second);
-                    swap(first_el,second_el);
-                }
+                
                 ct = first+1;
                 first=i;
-                first_el=fruits[i];
             }
-            else if(first_el==fruits[i])first=i;
-            else if(second_el==fruits[i])second=i;
+            else if(fruits[first]==fruits[i])first=i;
+            else if(fruits[second]==fruits[i])second=i;
             int p =i+1-ct;
             ans = max(ans,p);
             
