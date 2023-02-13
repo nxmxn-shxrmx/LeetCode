5828a19f334cca6aa754139d05ee9ab6d58f20d3
Naman Sharma <82465837+nxmxn-shxrmx@users.noreply.github.com>
2023-02-14 02:45:38 +0530

    Time: 2966 ms (5.00%), Space: 153.3 MB (23.13%) - LeetHub

--- 1898-maximum-number-of-removable-characters/1898-maximum-number-of-removable-characters.cpp
+++ 1898-maximum-number-of-removable-characters/1898-maximum-number-of-removable-characters.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
-    bool check(int k,string s,string p,vector<int>&v)
+    bool check(int k,string s,string p,map<int,int>&v)
     {
         int u = 0;
         for(int i = 0;i<s.size();++i)
         {
-            if(v[i]!=-1 && v[i]<k)
+            if(v.find(i)!=v.end() && v[i]<k)
                 continue;
             if(s[i]==p[u])
                 u++;
@@ -17,13 +17,13 @@ public:
       int l =0;
       int r =removable.size();
       int ans =l;
-      vector<int>splender(s.size(),-1);
-      for(int i = 0;i<removable.size();++i)splender[removable[i]]=i;
+      map<int,int>v;
+      for(int i = 0;i<removable.size();++i)v[removable[i]]=i;
         
      while(l<=r)
      {
          int m = (l+r)/2;
-         if(check(m,s,p,splender))
+         if(check(m,s,p,v))
              l = m+1,ans = m;
          else
              r = m-1;
