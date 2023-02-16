256dd2200f3fec3296b7250a06d4e0dfc612056c
Naman Sharma <82465837+nxmxn-shxrmx@users.noreply.github.com>
2023-02-16 14:28:41 +0530

    Time: 253 ms (60.08%), Space: 98.9 MB (99.90%) - LeetHub

--- 1838-frequency-of-the-most-frequent-element/1838-frequency-of-the-most-frequent-element.cpp
+++ 1838-frequency-of-the-most-frequent-element/1838-frequency-of-the-most-frequent-element.cpp
@@ -7,21 +7,14 @@ public:
         int j = v.size()-1;
         for(int i = v.size()-1;~i;--i)
         {
-            if(k-(v[j]-v[i])>=0)
-            {
-                k-=(v[j]-v[i]);
-                u = max(j-i+1,u);
-            }
-            else
-            {
                 k-=(v[j]-v[i]);
                 while(j>=i && k<0)
                 {
                     k +=(j-i)*(v[j]-v[j-1]);
                     j--;
                 }
-                  u = max(j-i+1,u);
-            }
+            u = max(j-i+1,u);
+            
         }
         
         return u;
