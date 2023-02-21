c46c3c7d21924a5cc59afe9051ad2a1c6e29d99f
Naman Sharma <82465837+nxmxn-shxrmx@users.noreply.github.com>
2023-02-22 00:14:09 +0530

    Time: 37 ms (22.29%), Space: 22.3 MB (62.74%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -2,29 +2,21 @@ class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
         if(nums.size()==1)return nums[0];
-        if(nums.size()!=1&&nums[0]!=nums[1])return nums[0];
-        if(nums.size()!=1&&nums[nums.size()-1]!=nums[nums.size()-2])return nums.back();
+        if(nums.back()!=nums[nums.size()-1])return nums.back();
+        if(nums[0]!=nums[1])return nums[0];
         int l = 0;
         int r = nums.size()-1;
         while(l<=r)
         {
-            int m = (l+r)/2;
-            if(nums[m]!=nums[m-1]&&nums[m]!=nums[m+1])return nums[m];
-            
-            if(nums[m]==nums[m-1])
-            {
-                if((r-m)%2==0)
-                    r = m-2;
-                else l = m+1;
-            }
-            else 
-            {
-                if((m-l)%2==0)
-                    l = m+2;
-                else r = m-1;
-            }
-           
+            int m= (l+r)/2;
+            if(nums[m]==nums[m+1])
+                m++;
+            if(nums[m]!=nums[m-1])return nums[m];
+            if((r-m)%2==1)
+                l = m+1;
+            else if((m-l)%2==0)
+                r = m -2;
         }
-        return nums[0];
+        return -1;
     }
 };
