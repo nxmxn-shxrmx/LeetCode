8f9b8ba1dddcdd1aea0f0957906fd51a46a19b31
Naman Sharma <82465837+nxmxn-shxrmx@users.noreply.github.com>
2023-03-06 18:14:09 +0530

    Time: 507 ms (5.04%), Space: 84.5 MB (5.04%) - LeetHub

---
+++ 1871-jump-game-vii/1871-jump-game-vii.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool canReach(string s, int minJump, int maxJump) {
+        queue<int>q;
+        q.push(0);
+        set<int>st;
+        for(int i =minJump;i<s.size();++i)if(s[i]=='0')st.insert(i);
+        while(!q.empty())
+        {
+            int i = q.front();
+            q.pop();
+            if(i==s.size()-1)return 1;
+            auto it = st.lower_bound(i+minJump);
+            while(it!=st.end() && (*it)<=(i+maxJump))
+            {
+                auto p = it;
+                ++p;
+                q.push(*it);
+                st.erase(*it);
+                it = p;
+            }
+        }
+        return 0;
+    }
+};
