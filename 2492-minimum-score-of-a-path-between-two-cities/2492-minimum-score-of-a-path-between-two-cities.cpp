3cd24130237ecaef2475ebffcb3902dc98166ea7
Naman Sharma <82465837+nxmxn-shxrmx@users.noreply.github.com>
2023-03-22 09:52:13 +0530

    Time: 427 ms (93.54%), Space: 114.6 MB (91.39%) - LeetHub

--- 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.cpp
+++ 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.cpp
@@ -1,34 +1,41 @@
 class Solution {
 public:
+    vector<int>par;
+    vector<int>size;
+    vector<int>mh;
+    int find(int i)
+    {
+        if(par[i]==-1)
+        {
+            return par[i]=i;
+        }
+        if(par[i]==i)return i;
+        return par[i] =find(par[i]);
+    }
     int minScore(int n, vector<vector<int>>& roads) {
-        vector<vector<pair<int,int>>>v(n+1);
+        par = vector<int>(n+1,-1);
+        size= vector<int>(n+1);
+        vector<int>mh(n+1,INT_MAX);
         for(auto c:roads)
         {
-            v[c[0]].push_back({c[1],c[2]});
-            v[c[1]].push_back({c[0],c[2]});
-        }
-        vector<int>dis(n+1,INT_MAX);
-        priority_queue<pair<int,int>>q;
-        q.push({INT_MAX,1});
-        dis[1]=0;
-        while(!q.empty())
-        {
-            int p = abs(q.top().first);
-            int i = q.top().second;
-            q.pop();
-            for(auto c:v[i])
+            int x = c[0];
+            int y = c[1];
+            int cos = c[2];
+            
+            int u = find(c[0]);
+            int v = find(c[1]);
+            if(u==v)
+                mh[u]= min(mh[u],cos);
+            
+            else
             {
-                if(dis[c.first]>(min(p,c.second)))
-                {
-                    dis[c.first] = min(p,c.second);
-                    q.push({-dis[c.first],c.first});
-                }
+                if(size[v]>size[u])swap(u,v);
+                par[v]=u;
+                size[u]+=size[v];
+                mh[u] = min(mh[u],mh[v]);
+                mh[u] = min(mh[u],cos);
             }
         }
-    
-        for(auto c:dis)
-            if(c!=0)
-            dis[n] = min(c,dis[n]);
-        return dis[n];
+        return mh[find(1)];
     }
 };
