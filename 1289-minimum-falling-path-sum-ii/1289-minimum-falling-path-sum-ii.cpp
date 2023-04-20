09333d6391a1510814f7b41b556124a529464777
Naman Sharma <82465837+nxmxn-shxrmx@users.noreply.github.com>
2023-04-20 15:27:05 +0530

    Time: 39 ms (91.46%), Space: 15.6 MB (64.94%) - LeetHub

--- 1289-minimum-falling-path-sum-ii/1289-minimum-falling-path-sum-ii.cpp
+++ 1289-minimum-falling-path-sum-ii/1289-minimum-falling-path-sum-ii.cpp
@@ -1,58 +1,58 @@
 class Solution {
 public:
-    int minFallingPathSum(vector<vector<int>>&m) {
-       
-        vector<pair<int,int>>v(2,{INT_MAX,0});
-        for(int i = 0;i<m.size();++i)
+    int minFallingPathSum(vector<vector<int>>& grid) {
+        pair<int,int>a;
+        pair<int,int>b;
+        a.first = 0;
+        a.second = -1;
+        b.first = 0;
+        b.second = -1;
+        for(auto x:grid)
         {
-            if(m[0][i]<=v[0].first)
+            pair<int,int>c;
+            c.first = INT_MAX;
+            c.second=-1;
+            pair<int,int>d;
+            d.second=-1;
+            d.first = INT_MAX;
+            for(int i = 0;i<x.size();++i)
             {
-                v[1]=v[0];
-                v[0].first=m[0][i];
-                v[0].second=i;
-            }
-            else if(m[0][i]<v[1].first)
-            {
-                v[1].first=m[0][i];
-                v[1].second=i;
-            }
-        }
-    
-        for(int i = 1;i<m.size();++i)
-        {
-            vector<pair<int,int>>q(2,{INT_MAX,0});
-            for(int j=0;j<m.size();++j)
-            {
-                int p = INT_MAX;
-                if(v[0].second!=j)
-                    p = v[0].first;
-                else
-                    p = v[1].first;
-                m[i][j]+=p;
-
-                if(m[i][j]<=q[0].first)
+                if(a.second!=i)
                 {
-                q[1]=q[0];
-                    q[0].first=m[i][j];
-                    q[0].second=j;
+                    if(a.first+x[i]<=c.first)
+                    {
+                        swap(d,c);
+                        c.first = a.first+x[i];
+                        c.second=i;
+                    }
+                    else if(a.first+x[i]<d.first)
+                    {
+                        d.first = a.first+x[i];
+                        d.second = i;
+                    }
                 }
-                else if(m[i][j]<=q[1].first)
+                else
                 {
-                    q[1].first=m[i][j];
-                    q[1].second=j;
+                    if(b.first+x[i]<=c.first)
+                    {
+                        swap(d,c);
+                        c.first = b.first+x[i];
+                        c.second=i;
+                    }
+                    else if(b.first+x[i]<d.first)
+                    {
+                        d.first = b.first+x[i];
+                        d.second = i;
+                    
+                    }
                 }
              
+             //   cout<<a.first<<" "<<a.second<<" "<<b.first<<" "<<b.second<<"\n";
             }
-              v[0]=q[0];
-            v[1]=q[1];
-            
+               a= c;
+                b = d;
         }
-        // for(auto c:m)
-        // {
-        //     for(auto x:c)
-        //         cout<<x<<" ";
-        //     cout<<"\n";
-        // }
-        return  v[0].first;
+             return min(a.first,b.first);
     }
+    
 };
