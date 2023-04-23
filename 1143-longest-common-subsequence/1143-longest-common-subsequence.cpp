5392ce466231bfae47c92731b02d2b004942ea16
Naman Sharma <82465837+nxmxn-shxrmx@users.noreply.github.com>
2023-04-23 21:05:15 +0530

    Time: 93 ms (16.87%), Space: 24.4 MB (56.89%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -1,24 +1,56 @@
 class Solution {
 public:
+    string  f(string &p,string &q)
+{
+	vector<vector<int>>v(p.size(),vector<int>(q.size()));
+	for(int i=  0;i<p.size();++i)
+	{
+		for(int j = 0;j<q.size();++j)
+		{
+			if(p[i]==q[j])
+			{
+				v[i][j]=1;
+				if(i-1>=0 && j-1>=0)
+					v[i][j]+=v[i-1][j-1];
+			}
+			else
+			{
+				if(i-1>=0)
+					v[i][j] = v[i-1][j];
+				if(j-1>=0)
+					v[i][j] = max(v[i][j],v[i][j-1]);
+			}
+		}
+	}
+	string s = "";
+	int i = p.size()-1;
+	int j = q.size()-1;
+	while(i>=0 && j>=0)
+	{
+		if(i>=0 && j>=0 && p[i]==q[j])
+		{
+			s+=p[i];
+			i-=1;
+			j-=1;
+		}
+		else 
+		{
+			if(i-1<0)
+				j-=1;
+			else if(j-1<0)
+				i-=1;
+			else if(v[i][j-1]>v[i-1][j])
+				j-=1;
+			
+			else
+				i-=1;
+		}
+	}
+	reverse(s.begin(),s.end());
+	return s;
+}
     int longestCommonSubsequence(string t1, string t2) {
-        int n = t1.size();
-        int m = t2.size();
-        
-        vector<int>cur(m+1,0);
-        vector<int>pre(m+1,0);
-        
-        for(int i = 1;i<=n;++i)
-        {
-            for(int j = 1;j<=m;++j)
-            {
-                if(t1[i-1]==t2[j-1])
-                    cur[j] = pre[j-1]+1;
-                else
-                    cur[j] = max(pre[j],cur[j-1]);
-            }
-            pre =cur;
-        }
-        
-        return pre[m];
+     
+        return f(t1,t2).size();
     }
 };
