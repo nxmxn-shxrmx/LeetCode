6dbc83c0da8899dba5eff36a8b8419426d91ffd6
Naman Sharma <82465837+nxmxn-shxrmx@users.noreply.github.com>
2023-04-24 22:50:48 +0530

    Time: 292 ms (65.98%), Space: 41.7 MB (8.85%) - LeetHub

--- 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
+++ 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
@@ -1,44 +1,23 @@
 class Solution {
 public:
     int dp[2001][2004];
+    int check[2001][2001];
     long long int f(int i,int j,string &s)
 {
 	if(i==s.size()-1)
 	{	
 		
-    bool k = 1;
-    int p = i;
-    int q = j;
-  //      cout<<p<<" "<<q<<"\n";
-    while(q<p)
-    {
-        
-        if(s[p]!=s[q])
-        {
-            k= 0;
-            break;
-        }
-        p--;
-        q++;
-    }
-  //      cout<<k<<"\n";
-        if(k)return 0;
+        if(s[i]==s[j]&&((i-j<2)||check[j+1][i-1]))return 0;
 		return INT_MAX;
 	}
 	if(dp[i][j]!=-1)return dp[i][j];
-    bool k = 1;
-    int p = i;
-    int q = j;
-    while(q<p)
-    {
-        if(s[p]!=s[q])
-        {
-            k= 0;
-            break;
-        }
-        p--;
-        q++;
-    }
+    bool k = 0;
+        
+            
+    if(s[i]==s[j]&&((i==j)||(i-j<2)||check[j+1][i-1]))k =1;
+    if(k)
+        check[j][i]=1;
+    
 	if(k)
 		return dp[i][j]= min(f(i+1,i+1,s)+1,f(i+1,j,s));
 	return dp[i][j]=f(i+1,j,s);	
