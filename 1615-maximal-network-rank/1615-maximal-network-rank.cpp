19e684ff2197532013582baa08c025e1364f4002
Naman Sharma <82465837+nxmxn-shxrmx@users.noreply.github.com>
2023-08-20 17:13:53 +0530

    Time: 116 ms (17.91%), Space: 43 MB (11.31%) - LeetHub

--- 1615-maximal-network-rank/1615-maximal-network-rank.cpp
+++ 1615-maximal-network-rank/1615-maximal-network-rank.cpp
@@ -1,26 +1,39 @@
 class Solution {
 public:
     int maximalNetworkRank(int n, vector<vector<int>>& roads) {
-       int h = INT_MIN;
-        vector<vector<int>>v(n);
+        vector<int>v(n);
         map<pair<int,int>,int>m;
         for(auto c:roads)
         {
-            v[c[0]].push_back(c[1]);
-            v[c[1]].push_back(c[0]);
+            v[c[0]]++;
+            v[c[1]]++;
             m[{c[0],c[1]}]=1;
             m[{c[1],c[0]}]=1;
         }
-        for(int i = 0;i<n;++i)
+        vector<pair<int,int>>vp;
+        for(int i =0;i<v.size();++i)
+            vp.push_back({v[i],i});
+        sort(vp.begin(),vp.end());
+        
+        int u= vp.back().first;
+        u+= vp[vp.size()-2].first;
+        if(vp.size()==1)return u;
+        if(m[{vp.back().second,vp[vp.size()-2].second}])u--;
+    
+        int h =u;
+        for(int i = vp.size()-1;i>=1;--i)
         {
-            for(int j=i+1;j<n;++j)
+            
+            for(int j =i-1;~j;--j)
             {
-                int p = v[i].size()+v[j].size();
-                if(m[{i,j}]==1)
-                    p--;
-                h = max(h,p);
+                
+                h = vp[i].first+vp[j].first;
+                if(m[{vp[i].second,vp[j].second}])h--;
+                if(h>u)u = h;
+                
+                else if(h<u)break;
             }
         }
-        return h;
+        return u;
     }
 };
