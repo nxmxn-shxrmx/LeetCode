c965496e388b58963544c353ac8ff4a2913ece91
Naman Sharma <82465837+nxmxn-shxrmx@users.noreply.github.com>
2023-08-20 23:17:56 +0530

    Time: 146 ms (13.95%), Space: 89.5 MB (58.60%) - LeetHub

--- 2369-check-if-there-is-a-valid-partition-for-the-array/2369-check-if-there-is-a-valid-partition-for-the-array.cpp
+++ 2369-check-if-there-is-a-valid-partition-for-the-array/2369-check-if-there-is-a-valid-partition-for-the-array.cpp
@@ -1,43 +1,21 @@
 class Solution {
 public:
-    int dp[100001][3];
-    bool f(int i,int ct,vector<int>&v)
+    int dp[100001];
+    bool f(int i,vector<int>&v)
     {
-        if(i==v.size()-1)
-        {
-            if(ct==2)
-            {
-                if(v[i]==(v[i-1]+1) && (v[i]==v[i-2]+2))return 1;
-                
-                if(v[i]==v[i-1] && v[i]==v[i-2])return 1;
-                
-                return 0;
-            }
-            if(ct==1 && v[i]==v[i-1])return 1;
-            
-            return 0;
-        }
-        if(dp[i][ct]!=-1)return dp[i][ct];
-        if(ct==0)
-            return dp[i][ct]=f(i+1,1,v);
-        if(ct==1)
-        {
-            if(v[i]==v[i-1])return dp[i][ct]= f(i+1,ct+1,v)||f(i+1,0,v);
-            if(v[i]==v[i-1]+1)return dp[i][ct]=f(i+1,ct+1,v);
-            
-            return dp[i][ct]=0;
-        }
         
-        if(v[i]==(v[i-1]+1) && (v[i]==v[i-2]+2))return dp[i][ct]= f(i+1,0,v);
-    
-         if(v[i]==v[i-1] && v[i]==v[i-2])return dp[i][ct]= f(i+1,0,v);
+        if(i==v.size())return 1;
         
-        return dp[i][ct]=0;       
+        if(dp[i]!=-1)return dp[i];
+        if(i+1<v.size() && v[i]==v[i+1]&&f(i+2,v))return 1;
+        if(i+2<v.size() && v[i]==v[i+1] && v[i]==v[i+2]&&f(i+3,v))return 1;
+        if(i+2<v.size() && v[i]+1==v[i+1] && v[i+1]+1==v[i+2] &&f(i+3,v))return 1;        
+        return dp[i]=0;
         
-           }
+    }
     bool validPartition(vector<int>& nums) {
         if(nums.size()==1)return 0;
         memset(dp,-1,sizeof(dp));
-        return f(0,0,nums);
+        return f(0,nums);
     }
 };
