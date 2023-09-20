d9bffc4714d89af24dfac1e02897ecb071c422c3
Naman Sharma <82465837+nxmxn-shxrmx@users.noreply.github.com>
2023-09-21 00:35:58 +0530

    Time: 139 ms (44.98%), Space: 17.7 MB (96.11%) - LeetHub

--- 1631-path-with-minimum-effort/1631-path-with-minimum-effort.cpp
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.cpp
@@ -2,8 +2,8 @@ class Solution {
 public:
     
     
-
-    int check(int i,int j,vector<vector<int>>&h,vector<vector<int>>&b,int m)
+    int b[101][101];
+    int check(int i,int j,vector<vector<int>>&h,int m)
     {
         if(i==h.size()-1 && h[0].size()-1==j)
                 return  1;
@@ -14,13 +14,13 @@ public:
         int ans = 0;
         for(auto c:d)
         {
-            if(i+c[0]>=0 && i+c[0]<h.size() && j+c[1]>=0 && j+c[1]<h[0].size() && m>=(abs(h[i][j]-h[i+c[0]][j+c[1]])) && !b[i+c[0]][j+c[1]])
+            if(i+c[0]>=0 && i+c[0]<h.size() && j+c[1]>=0 && j+c[1]<h[0].size() && m>=(abs(h[i][j]-h[i+c[0]][j+c[1]])))
             {
-             ans |=check(i+c[0],j+c[1],h,b,m);
+             ans |=check(i+c[0],j+c[1],h,m);
                 if(ans)return 1;
             }
         }
-
+        
         return 0;    
     }
     int minimumEffortPath(vector<vector<int>>& h) {
@@ -30,9 +30,8 @@ public:
         while(l<=r)
         {
             int m = (l+r)/2;
-            
-            vector<vector<int>>b(h.size(),vector<int>(h[0].size()));
-            if(check(0,0,h,b,m))
+            memset(b,0,sizeof(b));
+            if(check(0,0,h,m))
                 r = m-1 ,ans = m;
             else l = m+1;
         }
