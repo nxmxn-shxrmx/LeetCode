e1c436210db2e830a88a20f1559183d271d4bcad
Naman Sharma <82465837+nxmxn-shxrmx@users.noreply.github.com>
2024-01-21 23:07:23 +0530

    Time: 4 ms (25.45%), Space: 9.1 MB (9.67%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,16 +1,23 @@
 class Solution {
 public:
-    int dp[101];
-    int f(vector<int>&v,int i)
-    {
-        if(i>=v.size())return 0;
-        if(dp[i]!=-1)return dp[i];
-        return  dp[i]=max(f(v,i+1),f(v,i+2)+v[i]);
-    }
+    // int dp[101];
+    // int f(vector<int>&v,int i)
+    // {
+    //     if(i>=v.size())return 0;
+    //     if(dp[i]!=-1)return dp[i];
+    //     return  dp[i]=max(f(v,i+1),f(v,i+2)+v[i]);
+    // }
     int rob(vector<int>& nums) {
         
-        memset(dp,-1,sizeof(dp));
-        return f(nums,0);
+        vector<int>dp(nums.size()+1);
+        for(int i = 1;i<=nums.size();++i)
+        {
+            int g = 0;
+            if(i-2>=0)
+                g = dp[i-2];
+            dp[i] = max(dp[i-1],g+nums[i-1]);
+        }
+        return dp[nums.size()];
         // long long a=0,b=0;
         // for(int i = 0;i<nums.size();++i)
         // {
