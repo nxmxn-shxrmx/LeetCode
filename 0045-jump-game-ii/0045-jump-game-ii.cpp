238ceefc3081c41d4463f4dd8b181eb51d8e34ab
Naman Sharma <82465837+nxmxn-shxrmx@users.noreply.github.com>
2024-01-22 02:24:13 +0530

    Time: 11 ms (75.32%), Space: 19 MB (5.61%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -1,31 +1,18 @@
 class Solution {
 public:
-    long long dp[10001];
-    long long f(int i,vector<int>&v)
-    {
-        if(i>=v.size()-1)
-            return 0;
-        if(dp[i]!=-1)
-            return dp[i];
-        long long k = INT_MAX;
-        
-        for(int j =i+1;j<=v[i]+i;++j)
-            k = min(k,f(j,v)+1);
-        return dp[i]=k;
-    }
-    int jump(vector<int>&v) {
-    int r= 0;
-        int c = 0;
-            int e = 0;
-        for(int i = 0;i<v.size()-1;++i)
+    int jump(vector<int>& nums) {
+        int next = 0;
+        int atmost = 0;
+        int mini_jump =0;
+        for(int i = 0;i<nums.size()-1;++i)
         {
-            r = max(v[i]+i,r);
-            if(e==i)
+            atmost = max(i+nums[i],atmost);
+            if(next==i)
             {
-                c++;
-                e = r;
+                next=atmost;
+                mini_jump++;
             }
         }
-      return c;
+        return mini_jump;
     }
 };
