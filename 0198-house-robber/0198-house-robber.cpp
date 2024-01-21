39ec6408a640befe04f480663aad6493620877a5
Naman Sharma <82465837+nxmxn-shxrmx@users.noreply.github.com>
2024-01-21 23:07:50 +0530

    Time: 2 ms (40.56%), Space: 9 MB (9.67%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -9,27 +9,27 @@ public:
     // }
     int rob(vector<int>& nums) {
         
-        vector<int>dp(nums.size()+1);
-        for(int i = 1;i<=nums.size();++i)
-        {
-            int g = 0;
-            if(i-2>=0)
-                g = dp[i-2];
-            dp[i] = max(dp[i-1],g+nums[i-1]);
-        }
-        return dp[nums.size()];
-        // long long a=0,b=0;
-        // for(int i = 0;i<nums.size();++i)
+        // vector<int>dp(nums.size()+1);
+        // for(int i = 1;i<=nums.size();++i)
         // {
-        //     if(i&1)
-        //     {
-        //         a = max(nums[i]+a,b);
-        //     }
-        //     else
-        //     {
-        //         b =max(nums[i]+b,a);
-        //     }
+        //     int g = 0;
+        //     if(i-2>=0)
+        //         g = dp[i-2];
+        //     dp[i] = max(dp[i-1],g+nums[i-1]);
         // }
-        // return max(a,b);
+        // return dp[nums.size()];
+        long long a=0,b=0;
+        for(int i = 0;i<nums.size();++i)
+        {
+            if(i&1)
+            {
+                a = max(nums[i]+a,b);
+            }
+            else
+            {
+                b =max(nums[i]+b,a);
+            }
+        }
+        return max(a,b);
     }
 };
