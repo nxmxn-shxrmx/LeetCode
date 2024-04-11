8cf3da409480b41475a551e63a4cf19fa57a38ba
Naman Sharma <82465837+nxmxn-shxrmx@users.noreply.github.com>
2024-04-11 07:39:08 +0000

    Time: 3 ms (85.60%), Space: 16.2 MB (81.63%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
@@ -4,63 +4,15 @@ public:
         int las = 1;
         int cur = 1;
         int ans = INT_MIN;
-        for(auto c:nums)
+        for(int i = 0;i<nums.size();++i)
         {
-            if(c<0)
-            {
-                cur*=las*c;
-                ans = max(ans,cur);
-                if(cur<0)
-                {
-                    las = cur;
-                    cur = 1;
-                    
-                }
-                else
-                las =1;
-            }
-            else if(c==0)
-            {
-                las=1;
-                cur=1;
-                ans = max(ans,0);
-            }
-            else
-            {
-                cur*=c;
-                ans = max(ans,cur);
-            }
-        }
-        reverse(nums.begin(),nums.end());
-          las = 1;
-        cur = 1;
-    
-        for(auto c:nums)
-        {
-            if(c<0)
-            {
-                cur*=las*c;
-                ans = max(ans,cur);
-                if(cur<0)
-                {
-                    las = cur;
-                    cur = 1;
-                    
-                }
-                else
-                las =1;
-            }
-            else if(c==0)
-            {
-                las=1;
-                cur=1;
-                ans = max(ans,0);
-            }
-            else
-            {
-                cur*=c;
-                ans = max(ans,cur);
-            }
+            if(las==0)las = 1;
+            if(cur==0)cur = 1;
+            
+            las*=nums[i];
+            cur*=nums[nums.size()-1-i];
+            
+            ans= max(ans,max(las,cur));
         }
         return ans;
     }
