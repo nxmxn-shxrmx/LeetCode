53ea33dff1045f584fc9616d13a28d51ab837eb0
Naman Sharma <82465837+nxmxn-shxrmx@users.noreply.github.com>
2024-04-13 07:42:33 +0000

    Time: 14 ms (89.50%), Space: 60.4 MB (69.12%) - LeetHub

--- 0099-recover-binary-search-tree/0099-recover-binary-search-tree.cpp
+++ 0099-recover-binary-search-tree/0099-recover-binary-search-tree.cpp
@@ -11,44 +11,28 @@
  */
 class Solution {
 public:
-    vector<int>v;
-    vector<int>p;
+    TreeNode* pre= NULL;
+    TreeNode* fir = NULL;
+    TreeNode* sec = NULL;
     void in(TreeNode* r)
     {
         if(r==NULL)return;
         in(r->left);
        
-        v.push_back(r->val);
-        p.push_back(r->val);
+        if(pre&&pre->val>r->val)
+        {
+            if(fir==NULL)fir = pre;
+            sec = r;
+        }
+        pre =r;
         in(r->right);
     }
-    void f(TreeNode* &r,int a,int b)
-    {
-        if(r==NULL)return;
-        
-        if(r->val==a)r->val =b;
-        else if(r->val==b)r->val =a;
-        
-        f(r->left,a,b);
-        f(r->right,a,b);
-    }
+
     void recoverTree(TreeNode* root) {
         in(root);
       
-        sort(p.begin(),p.end());
-        int a = 0;
-        int b = 0;
-        for(int i = 0;i<p.size();++i)
-        {
-            if(p[i]!=v[i])
-            {
-                cout<<p[i]<<" ";
-                a = p[i];
-                b = v[i];
-                break;
-            }
-        }
-        f(root,a,b);
+        swap(fir->val,sec->val);
+        
         
     }
 };
