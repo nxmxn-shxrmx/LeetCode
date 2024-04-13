e83b5315565250fa3b4f74375149cc74912910b2
Naman Sharma <82465837+nxmxn-shxrmx@users.noreply.github.com>
2024-04-13 15:36:40 +0000

    Time: 28 ms (25.36%), Space: 26.6 MB (65.75%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
@@ -4,12 +4,19 @@ public:
   
         int n = m.size();
         vector<int>v(n,-1);
-        stack<int>s,r;
-        for(int i = n-2;~i;--i)
+        stack<int>s;
+        for(int i = n-1;~i;--i)
         {
+            
+                while(!s.empty() && s.top()<=m[i])
+                    s.pop();
+                if(!s.empty())
+                    v[i] = s.top();
+            
+            
             s.push(m[i]);
         }
-        for(int i = n-1;~i;--i)
+          for(int i = n-1;~i;--i)
         {
             
                 while(!s.empty() && s.top()<=m[i])
