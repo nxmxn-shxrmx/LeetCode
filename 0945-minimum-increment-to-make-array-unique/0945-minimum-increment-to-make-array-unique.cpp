ca0adfb8871560c354b3cb53dbe2cc4d425f0fe6
Naman Sharma <82465837+nxmxn-shxrmx@users.noreply.github.com>
2024-06-14 23:16:40 +0530

    Time: 101 ms (90.35%), Space: 69.2 MB (50.39%) - LeetHub

--- 0945-minimum-increment-to-make-array-unique/0945-minimum-increment-to-make-array-unique.cpp
+++ 0945-minimum-increment-to-make-array-unique/0945-minimum-increment-to-make-array-unique.cpp
@@ -1,45 +1,19 @@
 class Solution {
 public:
     int minIncrementForUnique(vector<int>& nums) {
-        set<int>s;
-        sort(nums.begin(),nums.end());
-        for(int i = 1;i<=nums[nums.size()-1];++i)s.insert(i);
-        for(int i =0;i<nums.size();++i)
+         sort(nums.begin(),nums.end());
+         int sum = 0;
+        for(int i =1;i<nums.size();++i)
         {
-            if(i-1>=0 && nums[i]==nums[i-1])
-                continue;
-            s.erase(nums[i]);
-        }
-        int sum = 0;
-        int u = nums[nums.size()-1]+1;
-        
-        for(int i = 0;i<nums.size();++i)
-        {
-            if(i-1>=0 && nums[i]==nums[i-1])
+            if(nums[i]<=nums[i-1])
             {
-                if(s.size()==0)
-                {
-                    sum += u-nums[i];
-                    u++;
-                }
-                else
-                {
-                    auto it = s.upper_bound(nums[i]);
-                    if(it!=s.end())
-                    {
-                        sum+=*it-nums[i];
-                        s.erase(*it);
-                    }
-                    else
-                    {
-                        s.clear();
-                        sum+=u-nums[i];
-                        u++;
-                    }
-                }
+                sum += nums[i-1]-nums[i]+1;
+                
+                nums[i] = nums[i-1]+1;
             }
         }
-        return sum;
+    
+    return sum;
     }
 };
 
